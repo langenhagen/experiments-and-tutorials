90304febd8bb994030e34dbf2467b9cb0ca27560
Andreas Langenhagen <andreas@langenhagen.cc>
2019-11-30 00:58:33 +0100

    Update pipefail-hello.sh

--- Bash/pipefail-hello.sh
+++ Bash/pipefail-hello.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # showcase usage of set -eo pipefail.
 # Run outside a git repository to make git status caue a non-zero error code.
+#
+# See:
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 git status 2>&1 | echo "a"
 echo 'after without pipefail will be printed'
 
-set -eo pipefail
+# -e: die on error;
+# pipefail: die after pipe finishes if intermediate steps in pipes exit nonzero
+# -E: take traps into consideration; set -e alone can avoid calling traps, e.g. on ERR
+set -Eeo pipefail
 git status 2>&1 | echo "b"
 echo 'after with pipefail will not be printed'
