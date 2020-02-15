30d20a8626de58551e2203c68633aee10116b326
Andreas Langenhagen <andreas@langenhagen.cc>
2020-02-15 23:30:50 +0100

    Add synthesizer-{keyboard,mouse}-hello.py

---
+++ Python/synthesizer-keyboard-hello.py
@@ -0,0 +1,33 @@
+#!/usr/env/bin python
+# author: andreasl
+"""Showcase the usage of the package synthesizer and pynput by making
+a sound by hitting any key on the keyboard.
+"""
+import pynput
+import synthesizer
+
+player = synthesizer.Player(rate=44100)
+player.open_stream()
+synthesizer = synthesizer.Synthesizer(
+    osc1_waveform=synthesizer.Waveform.sine,
+    osc1_volume=1.0,
+    use_osc2=False)
+
+
+def on_press(key):
+    """Play a note."""
+    freq_Hz = hash(key) % 500 + 100.0
+    duration_s = 30.0 / freq_Hz
+    wave = synthesizer.generate_constant_wave(freq_Hz, duration_s)
+    player.play_wave(wave)
+
+
+def on_release(key):
+    """Always return true, i.e. keep listener running."""
+    del key
+    return True
+
+
+Listener = pynput.keyboard.Listener
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
---
+++ Python/synthesizer-mouse-hello.py
@@ -0,0 +1,40 @@
+#!/usr/env/bin python
+# author: andreasl
+"""Showcase the usage of the package synthesizer and pynput by making
+a sound by moving the mouse.
+"""
+import pynput
+import synthesizer
+
+player = synthesizer.Player(rate=44100)
+player.open_stream()
+synthesizer = synthesizer.Synthesizer(
+    osc1_waveform=synthesizer.Waveform.sine,
+    osc1_volume=1.0,
+    use_osc2=False)
+
+
+def on_move(x, y):
+    """Play a note."""
+    freq_Hz = x + y + 1.0
+    duration_s = 30.0 / freq_Hz
+    wave = synthesizer.generate_constant_wave(freq_Hz, duration_s)
+    player.play_wave(wave)
+
+
+def on_click(x, y, button, pressed):
+    print(f"{button} {'pressed' if pressed else 'released'} at {(x,y)}")
+    # if not pressed:
+    #     # Stop listener
+    #     return False
+
+
+def on_scroll(x, y, dx, dy):
+    print(f"Scrolled {(x, y, dx, dy)}")
+
+
+with pynput.mouse.Listener(
+        on_move=on_move,
+        on_click=on_click,
+        on_scroll=on_scroll) as listener:
+    listener.join()
