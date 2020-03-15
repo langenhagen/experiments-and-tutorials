e0f6c4cf5b8706165174f4c3a1228c8f04fac5de
Andreas Langenhagen <andreas@langenhagen.cc>
2020-03-15 23:47:27 +0100

    Add key-combinations to x11 experiment
    Add support for ctrl+enter and make some refactorings.
    
    Unrelated to that, sometimes, there seem to be issues with window width
    scaling so that the trailing letters exceed the window width a small
    bit.

--- Cpp/X11-hello/04-x11-multiple-text-lines-hello/main.cpp
+++ Cpp/X11-hello/04-x11-multiple-text-lines-hello/main.cpp
@@ -1,6 +1,6 @@
 /* Render an X11 window and adjust its size to the amount of text and lines that you type.
 
-- based on my prior learnings
+- based on my prior learnings and the source code of dmenu.
 
 - use the command line tool `xev` to find out x11 key codes
 
@@ -12,13 +12,19 @@ author: andreasl
 #include <algorithm>
 #include <cstring>
 #include <iostream>
+#include <vector>
 #include <thread>
 
-/* order of X11 includes allegedlly important*/
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
 #include <X11/Xft/Xft.h>
+
+struct Line {
+    static const unsigned int buffer_size = 255;
+    unsigned int length;
+    char buffer[buffer_size];
+};
 
 struct App {
      /*x11 essentials*/
@@ -28,23 +34,24 @@ struct App {
     Window window;
     GC gc;
 
-     /*Xft stuff*/
+    /*Xft stuff*/
     XftDraw *xft_drawable;
     XftFont* font;
 
     /*application stuff*/
-    unsigned int line_height;
+    unsigned int height = 1;
     unsigned int width = 1;
-    unsigned int height = 20;
-    int n_redraws = 0;
-    static const int text_buffer_size = 255;
-    char text_buffer[text_buffer_size];
-    int text_cursor_pos = 0;
+    unsigned int line_height;
+    std::vector<Line> lines;
+    unsigned int text_cursor_row = 0;
+    unsigned int text_cursor_col = 0;
+    bool is_ctrl_pressed = false;
+    unsigned long window_background_color = 0x000000;
 };
 
 static void grab_keyboard(App *app) {
     using namespace std::chrono_literals;
-    /* try to grab keyboard. 1000 times.
+    /* try to grab keyboard 1000 times.
        we may have to wait for another process to ungrab*/
     for (int i = 0; i < 1000; ++i) {
         int grab_result = XGrabKeyboard(
@@ -71,13 +78,13 @@ static App* setup_x() {
 
     grab_keyboard(app);
 
-    unsigned long black = BlackPixel(app->display, app->screen);
-    unsigned long white = WhitePixel(app->display, app->screen);
-
     XSetWindowAttributes set_window_attributes;
     set_window_attributes.override_redirect = True; /*if True, window manager doesn't mess with the window*/
-    set_window_attributes.background_pixel = 0x000000;  /*rgb values*/
-    set_window_attributes.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask;
+    set_window_attributes.background_pixel = app->window_background_color;  /*rgb values*/
+    set_window_attributes.event_mask = ExposureMask
+        | KeyPressMask
+        | KeyReleaseMask
+        | VisibilityChangeMask;
 
     app->window = XCreateWindow(
         app->display /*display*/,
@@ -105,6 +112,7 @@ static App* setup_x() {
 
     app->gc = XCreateGC(app->display, app->window, 0, 0);
 
+    unsigned long white = WhitePixel(app->display, app->screen);
     XSetForeground(app->display, app->gc, white);
     XClearWindow(app->display, app->window);
     XMapRaised(app->display, app->window);
@@ -134,24 +142,39 @@ static void setup_xft_font(App *app) {
         XFT_SIZE,
         XftTypeDouble,
         20.0,
-        nullptr );
+        nullptr);
 
     app->line_height = app->font->ascent + app->font->descent;
 }
 
-static void resize_window(App *app) {
-    XGlyphInfo glyph_info;
-    XftTextExtents8(
-        app->display/*Display*/,
-        app->font /*xftfont*/,
-        (XftChar8 *)app->text_buffer /*string*/,
-        app->text_cursor_pos /*int len*/,
-        &glyph_info /*out glyph info*/);
+static void adjust_window_width(App *app) {
+    unsigned int window_width = 1;
+    for(auto line : app->lines) {
+        XGlyphInfo glyph_info;
+        XftTextExtents8(
+            app->display/*Display*/,
+            app->font /*xftfont*/,
+            (XftChar8 *)line.buffer /*string*/,
+            line.length /*int len*/,
+            &glyph_info /*out glyph info*/);
 
-    const unsigned int window_width = glyph_info.width;
+        if(glyph_info.width > window_width) {
+            window_width = glyph_info.width;
+        }
+    }
 
-    app->width = std::max(window_width, 1u);
-    app->height = std::max(app->line_height, 1u);
+    if(app->width != window_width) {
+        app->width = window_width;
+        XResizeWindow(
+            app->display /*display*/,
+            app->window /*window*/,
+            window_width /*width*/,
+            app->height /*height*/);
+    }
+}
+
+static void adjust_window_height(App *app) {
+    app->height = app->line_height * app->lines.size() + 1;
 
     XResizeWindow(
         app->display /*display*/,
@@ -175,15 +198,17 @@ static void draw_text(App *app) {
         &x_render_color /*color*/,
         &xft_color /*result*/);
 
-    XftDrawString8(
-        app->xft_drawable /*drawable*/,
-        &xft_color /*color*/,
-        app->font /*font*/,
-        0 /*pos x*/,
-        20 /*pos y*/,
-        (unsigned char*)app->text_buffer,
-        app->text_cursor_pos);
-
+    for(size_t i = 0; i < app->lines.size(); ++i) {
+        auto& line = app->lines[i];
+        XftDrawString8(
+            app->xft_drawable /*drawable*/,
+            &xft_color /*color*/,
+            app->font /*font*/,
+            0 /*pos x*/,
+            app->line_height * i + app->font->ascent /*pos y*/,
+            (unsigned char*)line.buffer,
+            line.length);
+    }
     XftColorFree(
         app->display,
         DefaultVisual(app->display, 0),
@@ -193,7 +218,6 @@ static void draw_text(App *app) {
 
 static void redraw(App *app) {
     XClearWindow(app->display, app->window);
-    resize_window(app);
     draw_text(app);
 }
 
@@ -205,6 +229,7 @@ static int run(App *app) {
     while(!XNextEvent(app->display, &event)) {
         // if (XFilterEvent(&event, app->window))
         //     continue;
+        unsigned int key_code = 0;
         switch(event.type) {
         case Expose:
             if(event.xexpose.count == 0) {
@@ -212,28 +237,59 @@ static int run(App *app) {
             }
             break;
         case KeyPress:
-            if(event.xkey.keycode == 9 /*esc*/) {
+            key_code = event.xkey.keycode;
+            if(key_code == 9 /*esc*/) {
                 return 0;
-            }
-            else if(event.xkey.keycode == 22 /*delete*/) {
-                app->text_cursor_pos = std::max(--app->text_cursor_pos, 0);
+            } else if(key_code == 22 /*delete*/) {
+                auto &line = app->lines.back();
+                if(line.length == 0 && app->lines.size() > 1) {
+                    app->lines.pop_back();
+                    adjust_window_height(app);
+                    redraw(app);
+                } else if(line.length > 0) {
+                    --line.length;
+                    adjust_window_width(app);
+                    redraw(app);
+                }
+            } else if(key_code == 37 /*ctrl left*/  || key_code == 105 /*ctrl + right*/) {
+                app->is_ctrl_pressed = true;
+            } else if(key_code == 36 /*enter*/) {
+                if(app->is_ctrl_pressed) {
+                    if ( app->window_background_color == 0x000000) {
+                        app->window_background_color = 0x550000;
+                    } else {
+                        app->window_background_color = 0x000000;
+                    }
+                    XSetWindowBackground(app->display, app->window, app->window_background_color);
+                } else {
+                    app->lines.push_back(Line());
+                    adjust_window_height(app);
+                }
                 redraw(app);
-            }
-            else if(XLookupString(
-                &event.xkey,
-                input_buffer,
-                input_buffer_size,
-                nullptr,
-                nullptr) > 0) {
+            } else if(XLookupString(
+                    &event.xkey,
+                    input_buffer,
+                    input_buffer_size,
+                    nullptr,
+                    nullptr) > 0) {
                 /* normal text input*/
-                app->text_buffer[app->text_cursor_pos] = input_buffer[0];
-                ++app->text_cursor_pos;
+                Line &line = app->lines.back();
+                line.buffer[line.length] = input_buffer[0];
+                ++line.length;
+                adjust_window_width(app);
                 redraw(app);
             }
             break;
+        case KeyRelease:
+            key_code = event.xkey.keycode;
+            if(key_code == 37 /*ctrl left*/  || key_code == 105 /*ctrl + right*/) {
+                app->is_ctrl_pressed = false;
+            }
+            break;
         case VisibilityNotify:
-            if (event.xvisibility.state != VisibilityUnobscured)
+            if (event.xvisibility.state != VisibilityUnobscured) {
                 XRaiseWindow(app->display, app->window);
+            }
             break;
         }
     }
@@ -242,6 +298,8 @@ static int run(App *app) {
 int main(int argc, const char* argv[]) {
     App *app = setup_x();
     setup_xft_font(app);
+    app->lines.push_back(Line());
+    adjust_window_height(app);
     run(app);
     close_x(app);
     return 0;
