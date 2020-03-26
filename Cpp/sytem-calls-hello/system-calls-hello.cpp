049836e2c7ff3d1113d8f3ab2f3f2493566c6fa0
Andreas Langenhagen <andreas@langenhagen.cc>
2020-03-27 00:13:31 +0100

    Refactor system-calls-hello.cpp

--- Cpp/sytem-calls-hello/system-calls-hello.cpp
+++ Cpp/sytem-calls-hello/system-calls-hello.cpp
@@ -5,8 +5,8 @@ see: https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-g
 note: popen() apparently catches stdout, but not stderr.
 */
 #include <array>
+#include <cstdio>  // FILE, popen
 #include <cstdlib>  // system
-#include <cstdio> // FILE, popen
 #include <iostream>
 #include <memory>
 
@@ -16,7 +16,6 @@ int main () {
     std::cout << "Command has been run with exit code: " << return_value << "\n";
 
     std::cout << "\n\nRunning command via unix's popen()...\n";
-    std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(
         popen("for i in $(seq 3 9); do echo $i; sleep 1; done;", "r"),
@@ -24,6 +23,7 @@ int main () {
     if(!pipe) {
         throw std::runtime_error("Error: call to popen() failed");
     }
+    std::array<char, 128> buffer;
     while(std::fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
         std::cout << "> " << buffer.data();
         result += buffer.data();
