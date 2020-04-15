0a0de69f4c387c6d3a9567fdac58c0eb0398f1db
Andreas Langenhagen <andreas@langenhagen.cc>
2020-04-15 22:27:19 +0200

    Generalize cmake files for yaml-cpp-hello
    Generalize the cmake files for yaml-cpp-hello in order to have cmake
    files that are more reusable, i.e. copy-pasteable.

--- Cpp/yaml-cpp-hello/cmake/yaml-cpp.cmake
+++ Cpp/yaml-cpp-hello/cmake/yaml-cpp.cmake
@@ -10,28 +10,30 @@
 #
 # author: andreasl
 
-configure_file(
-    "${CMAKE_CURRENT_LIST_DIR}/yaml-cpp-CMakeLists.txt.in"
-    "yaml-cpp-download/CMakeLists.txt")
+set(EXT_PROJECT_NAME "yaml-cpp")
 
-# Download yaml-cpp sources
+configure_file(
+    "${CMAKE_CURRENT_LIST_DIR}/${EXT_PROJECT_NAME}-CMakeLists.txt.in"
+    "${EXT_PROJECT_NAME}-download/CMakeLists.txt")
+
+# Download sources
 execute_process(
     COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
     RESULT_VARIABLE result
-    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/yaml-cpp-download")
+    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/${EXT_PROJECT_NAME}-download")
 if(result)
-  message(FATAL_ERROR "CMake step for yaml-cpp-download failed: ${result}")
+  message(FATAL_ERROR "CMake step for ${EXT_PROJECT_NAME}-download failed: ${result}")
 endif()
 execute_process(
     COMMAND ${CMAKE_COMMAND} --build .
     RESULT_VARIABLE result
-    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/yaml-cpp-download")
+    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/${EXT_PROJECT_NAME}-download")
 if(result)
-  message(FATAL_ERROR "Build step for yaml-cpp-download failed: ${result}")
+  message(FATAL_ERROR "Build step for ${EXT_PROJECT_NAME}-download failed: ${result}")
 endif()
 
-# Build yaml-cpp
-set(build_dir "${CMAKE_BINARY_DIR}/yaml-cpp")
+# Build
+set(build_dir "${CMAKE_BINARY_DIR}/${EXT_PROJECT_NAME}")
 file(MAKE_DIRECTORY "${build_dir}")
 execute_process(
     COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}"
@@ -39,20 +41,20 @@ execute_process(
         -DYAML_CPP_BUILD_TOOLS=OFF
         -DYAML_CPP_INSTALL=OFF
         -DYAML_BUILD_SHARED_LIBS=OFF
-        "${CMAKE_BINARY_DIR}/yaml-cpp-src"
+        "${CMAKE_BINARY_DIR}/${EXT_PROJECT_NAME}-src"
     RESULT_VARIABLE result
     WORKING_DIRECTORY "${build_dir}")
 if(result)
-  message(FATAL_ERROR "CMake step for yaml-cpp failed: ${result}")
+  message(FATAL_ERROR "CMake step for ${EXT_PROJECT_NAME} failed: ${result}")
 endif()
 execute_process(COMMAND ${CMAKE_COMMAND} --build .
     RESULT_VARIABLE result
     WORKING_DIRECTORY "${build_dir}")
 if(result)
-  message(FATAL_ERROR "Build step for yaml-cpp failed: ${result}")
+  message(FATAL_ERROR "Build step for ${EXT_PROJECT_NAME} failed: ${result}")
 endif()
 
-set(lib_filename "${CMAKE_STATIC_LIBRARY_PREFIX}yaml-cpp${CMAKE_STATIC_LIBRARY_SUFFIX}")
+set(lib_filename "${CMAKE_STATIC_LIBRARY_PREFIX}${EXT_PROJECT_NAME}${CMAKE_STATIC_LIBRARY_SUFFIX}")
 
-set(YAML_CPP_LIB "${CMAKE_BINARY_DIR}/yaml-cpp/${lib_filename}")
-set(YAML_CPP_INCLUDE_PATH "${CMAKE_BINARY_DIR}/yaml-cpp-src/include")
+set(YAML_CPP_LIB "${CMAKE_BINARY_DIR}/${EXT_PROJECT_NAME}/${lib_filename}")
+set(YAML_CPP_INCLUDE_PATH "${CMAKE_BINARY_DIR}/${EXT_PROJECT_NAME}-src/include")
