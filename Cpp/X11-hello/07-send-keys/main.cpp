c52a9c2eb8430632eee5856d4729856956a99478
Andreas Langenhagen <andreas@langenhagen.cc>
2020-05-03 22:34:40 +0200

    Improve x11 experiment 07-send-keys

--- Cpp/X11-hello/07-send-keys/main.cpp
+++ Cpp/X11-hello/07-send-keys/main.cpp
@@ -1,9 +1,15 @@
-/* TODO
+/* Showcase sending of key events to the x window system.
 
 A full list of available key codes can be found in /usr/include/X11/keysymdef.h
 
+based on:
+    - https://ubuntuforums.org/showthread.php?t=2190400
+    - https://web.archive.org/web/20070712224859/http://www.doctort.org/adam/nerd-notes/x11-fake-keypress-event.html
 
-based on: https://ubuntuforums.org/showthread.php?t=2190400
+
+see also:
+    - https://www.x.org/releases/X11R7.5/doc/man/man3/XSync.3.html
+    - https://stackoverflow.com/questions/30413316/sending-fake-keypress-event-to-a-window-using-xlib
 
 author: andreasl
 */
@@ -31,7 +37,7 @@ XKeyEvent create_key_event(
     event.same_screen = True;
     event.keycode = XKeysymToKeycode(display, keycode);
     event.state = modifiers;
-    event.type = press ? KeyPress : KeyRelease;
+    event.type = press == true ? KeyPress : KeyRelease;
     return event;
 }
 
@@ -44,33 +50,31 @@ main() {
     int revert;
     XGetInputFocus(display, &focused_win, &revert);
 
-    XEvent evt;
-
     /*Ctrl + l*/
     /*Send a fake key press event to the focused window*/
-    evt.xkey = create_key_event(display, focused_win, root, true, XK_l, ControlMask);
-    XSendEvent(evt.xkey.display, evt.xkey.window, True, KeyPressMask, &evt);
+    XEvent focus_evt;
+    focus_evt.xkey = create_key_event(display, focused_win, root, true, XK_l, ControlMask);
+    XSendEvent(focus_evt.xkey.display, focus_evt.xkey.window, True, KeyPressMask, &focus_evt);
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    /*Send a fake key release event to the window*/
+    focus_evt.xkey = create_key_event(display, focused_win, root, false, XK_l, ControlMask);
+    XSendEvent(focus_evt.xkey.display, focus_evt.xkey.window, True, KeyPressMask, &focus_evt);
 
-    // /*Send a fake key release event to the window*/
-    // evt.xkey= create_key_event(display, focused_win, root, false, XK_l, ControlMask);
-    // XSendEvent(evt.xkey.display, evt.xkey.window, True, KeyPressMask, &evt);
+    /*both flushing and sleeping a very short amount of time seems to be necessary.*/
+    XFlush(display);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    // /*Ctrl + c*/
+    // /*Send a fake key press event to the focused window*/
+    XEvent copy_evt;
+    copy_evt.xkey = create_key_event(display, focused_win, root, true, XK_c, ControlMask);
+    XSendEvent(copy_evt.xkey.display, copy_evt.xkey.window, True, KeyPressMask, &copy_evt);
 
-    /*Ctrl + c*/
-    /*Send a fake key press event to the focused window*/
-    evt.xkey = create_key_event(display, focused_win, root, true, XK_x, ControlMask);
-    XSendEvent(evt.xkey.display, evt.xkey.window, True, KeyPressMask, &evt);
+    copy_evt.xkey = create_key_event(display, focused_win, root, false, XK_c, ControlMask);
+    XSendEvent(copy_evt.xkey.display, copy_evt.xkey.window, True, KeyPressMask, &copy_evt);
 
-    // std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
-    // /*Send a fake key release event to the window*/
-    // evt.xkey = create_key_event(display, focused_win, root, false, XK_c, ControlMask);
-    // XSendEvent(evt.xkey.display, evt.xkey.window, True, KeyPressMask, &evt);
-
-    // std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    XFlush(display);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
     XCloseDisplay(display);
     return 0;
