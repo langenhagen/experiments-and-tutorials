97d2432cd3d08721917b08c72ec5cd40430bc0ab
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-05-17 19:28:10 +0200

    Add documentation/comments to valgrind-hello.cpp

--- Cpp/valgrind-hello.cpp
+++ Cpp/valgrind-hello.cpp
@@ -1,13 +1,33 @@
-  #include <stdlib.h>
+/*Showcase some issues that valgrind can detect.
 
-  void f(void)
-  {
-     int* x = malloc(10 * sizeof(int));
-     x[10] = 0;        // problem 1: heap block overrun
-  }                    // problem 2: memory leak -- x not freed
+Wrap your program call with valgrind to check the program while it is running.
 
-  int main(void)
-  {
-     f();
-     return 0;
-  }
+Install valgrind like:
+  sudo apt install -y valgrind
+
+Valgrind example usage in Fish Shell:
+
+set G_SLICE always-malloc;
+set G_DEBUG gc-friendly;
+
+valgrind \
+    -v \
+    --tool=memcheck \
+    --leak-check=full \
+    --num-callers=40 \
+    --log-file=valgrind.log \
+    $PWD/08-my-text-input-field
+
+author: andreasl
+*/
+#include <stdlib.h>
+
+void f(void) {
+   int* x = malloc(10 * sizeof(int));
+   x[10] = 0;        // problem 1: heap block overrun
+}                    // problem 2: memory leak -- x not freed
+
+int main(void) {
+   f();
+   return 0;
+}
