0e810ca36cbd413c203bcb1d22c961ae816072ff
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-06-17 23:24:55 +0200

    Add comments to beautifulsoup-hello-website-text-scrape.py

--- Python/beautifulsoup-hello-website-text-scrape.py
+++ Python/beautifulsoup-hello-website-text-scrape.py
@@ -1,15 +1,19 @@
 #!/usr/env/bin python3
-"""Scrape the text from a website using beautifulsoup."""
+"""Scrape the text from a website using beautifulsoup.
+
+Simple but flawed. Also scrapes the javascript-scripts.
+"""
 import re
 
 import bs4
 import requests
 
 response = requests.get("https://langenhagen.github.io/recipes/Waffeln")
+# response = requests.get("https://google.com")
 soup = bs4.BeautifulSoup(response.text, "html5lib")
 
 website_text = soup.get_text()  # has linebreaks "\n" escaped as b"\\n"
-print(f"Type: {type(website_text)}")
+print(f"Type: {type(website_text)}")  #  Type: <class 'str'>
 
 # remove excessive newlines
 website_text_compacted = re.sub(r" *\n( *\n)+", r"\n\n", website_text)
