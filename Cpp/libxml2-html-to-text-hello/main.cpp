4ed5d86c91d67b4e7db1961ee7f4d7bba4147e0c
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-06-17 23:25:23 +0200

    Finish libxml2-html-to-text-hello
    Finally works for the obvious part. Actually even better than the simple
    beautifulsoup experiment, which would also print js-scripts from
    <script> tags.

--- Cpp/libxml2-html-to-text-hello/main.cpp
+++ Cpp/libxml2-html-to-text-hello/main.cpp
@@ -5,9 +5,12 @@ author: andreasl
 based on:
     - https://stackoverflow.com/questions/10740250/c-cpp-version-of-beautifulsoup-especially-at-handling-malformed-html
     - http://www.xmlsoft.org/html/libxml-HTMLparser.html
+    - http://xmlsoft.org/html/libxml-xmlerror.html#xmlParserErrors
 */
+#include <cstring>
 #include <iostream>
 #include <sstream>
+#include <string>
 
 #include <curl/curl.h>
 #include <libxml2/libxml/encoding.h>
@@ -15,43 +18,49 @@ based on:
 
 /*Error code when downloading a web site.*/
 enum class DownloadResultErrorCode {
-    OK = 0,
-    FAILED_INIT_CURL = 1,
-    CURL_ERROR = 2
+    OK = 0  /*No error*/,
+    FAILED_INIT_CURL = 1  /*Initializing CURL failed*/,
+    CURL_ERROR = 2  /*Downloading via CURL failed*/
 };
 
 /*Result of downloading a website.*/
 struct DownloadResult {
-    DownloadResultErrorCode code;  /*Download result.*/
+    DownloadResultErrorCode code;  /*Download result error code.*/
     CURLcode curl_code;  /*Curl error code.*/
     std::string content;  /*Downloaded content.*/
 };
 
+/*Parse the chunk of incoming data.*/
 size_t curl_callback(void* ptr, const size_t size, const size_t nmemb, htmlParserCtxtPtr* parser) {
-    std::cout
-        << "htmlParseChunk(*parser, reinterpret_cast<char*>(ptr), " << nmemb << ", 0) = "
-        << htmlParseChunk(*parser, reinterpret_cast<char*>(ptr), nmemb, 0)
-        << std::endl;
+    htmlParseChunk(*parser, reinterpret_cast<char*>(ptr), nmemb, 0);
     return size * nmemb;
 }
 
-void walk_tree(xmlNode* a_node) {
-    std::cout << "walk_tree() a_node: " << a_node << "\n";
-
-    xmlNode* cur_node = nullptr;
-    xmlAttr* cur_attr = nullptr;
-    for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
-        // do something with that node information, like... printing the tag's name and attributes
-        std::cout << "Got tag : " << cur_node->name << std::endl;
-        for (cur_attr = cur_node->properties; cur_attr; cur_attr = cur_attr->next) {
-            std::cout << "  ->; with attribute : " << cur_attr->name << std::endl;
+/*Check whether the given xml node contains children that contain non-visible text.*/
+bool check_if_node_is_safe_to_print(const xmlNode* root) {
+    for (const xmlNode* node = root; node; node = node->next) {
+        if (!node->name
+            || std::strcmp(reinterpret_cast<const char*>(node->name), "script") == 0
+            || !check_if_node_is_safe_to_print(node->children)) {
+            return false;
         }
-        walk_tree(cur_node->children);
+    }
+    return true;
+}
+
+/*Iterate over the xml tree and write printable stuff to the given stringstream.*/
+void walk_xml_tree(const xmlNode* root, std::ostringstream& oss) {
+    for (const xmlNode* node = root; node; node = node->next) {
+        if (!check_if_node_is_safe_to_print(node)) {
+            walk_xml_tree(node->children, oss);
+            continue;
+        }
+        oss << reinterpret_cast<char*>(xmlNodeGetContent(node)) << " ";
     }
 }
 
-/*Download a website.*/
-DownloadResult get_website(const std::string& url) {
+/*Download a website's text.*/
+DownloadResult get_website_text(const std::string& url) {
     CURL* curl = curl_easy_init();
     if (!curl) {
         std::cerr << "Failed to init CURL" << std::endl;
@@ -77,24 +86,29 @@ DownloadResult get_website(const std::string& url) {
     CURLcode res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
         std::cerr << "Error: curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-        return {DownloadResultErrorCode::CURL_ERROR, res, "TODO"};
+        return {DownloadResultErrorCode::CURL_ERROR, res, ""};
     }
     curl_easy_cleanup(curl);
 
-    std::cout
-            << "htmlParseChunk(parser, nullptr, 0, 1) = "
-            << htmlParseChunk(parser, nullptr, 0, 1)
-            << std::endl;
-    std::cout << parser->myDoc << std::endl;
+    /*//following would be enough if it weren't for the non-visible stuff, like `script` nodes
+    std::string s(reinterpret_cast<char*>(xmlNodeGetContent(xmlDocGetRootElement(parser->myDoc))));
+    std::cout << "content: " << s << std::endl;
+    /**/
 
-    walk_tree(xmlDocGetRootElement(parser->myDoc));
-
-    return {DownloadResultErrorCode::OK, CURLE_OK, "TODO"};
+    std::ostringstream oss;
+    walk_xml_tree(xmlDocGetRootElement(parser->myDoc), oss);
+    return {DownloadResultErrorCode::OK, CURLE_OK, oss.str()};
 }
 
 int main(int argc, const char* argv[]) {
-
-    get_website("https://example.com");
-
+    if (argc > 1) {
+        for(int i=1; i<argc; ++i) {
+            DownloadResult result  = get_website_text(argv[i]);
+            std::cout << result.content << "\n=======================================" <<std::endl;
+        }
+    } else {
+        DownloadResult result  = get_website_text("https://amazon.com");
+        std::cout << result.content << std::endl;
+    }
     return 0;
 }
