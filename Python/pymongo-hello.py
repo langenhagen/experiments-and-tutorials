78dcc8532d6660b4edad68ea4cca0ab24f05d327
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-07-09 14:41:58 +0200

    Add pymongo-hello.py

---
+++ Python/pymongo-hello.py
@@ -0,0 +1,19 @@
+#!/usr/env/bin python
+"""Showcase the usage of PyMongo.
+
+get it via `pip install pymongo`
+"""
+
+import pymongo
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+
+db = client["non-existing"]  # accessing non-existing dbs works
+db = client.non_existing  # shortcut; works only for with allowed characters; works also with non-existing; don't use it
+db = client["c1_cre"]
+
+collection = db["non-existing"]  # accessing non-existing collections works
+collection = db.mynonexisting  # shortcut; works also for non-existing collections; don't use it
+collection = db["sso_page_template"]
+for doc in collection.find({}):
+    print(doc["name"])
