69f5fd67a3e36c40c05d23dea0c8c08f3bdbaf8b
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-07-30 22:02:55 +0200

    Add classmethods-and-subclassing.py

---
+++ Python/classmethods-and-subclassing.py
@@ -0,0 +1,41 @@
+#!/usr/env/bin python
+"""Showcase the behavior of @classmethods in the light of subclassing.
+Sublclassing is possible, cls.__names__ match the actually instantiated classes/instances.
+Basically straightforward & what I hoped for.
+"""
+
+
+class C(object):
+    @classmethod
+    def foo(cls,a,b):
+        print( "C " + str(cls.__name__) + " a=" + str(a) + " b=" + str(b))
+
+
+class D(C):
+    @classmethod
+    def foo(cls,a,b):
+        print("D " + str(cls.__name__) + " a=" + str(a) + " b=" + str(b))
+
+
+class E(C):
+    pass
+
+
+print("--- 1 - calls via classes ---")
+
+print("\nC.foo(1,2):")
+C.foo(1,2)
+
+print("\nD.foo(1,2):")
+D.foo(1,2)
+
+print("\nE.foo(1,2):")
+E.foo(1,2)
+
+print("\n--- 2 - calls via instances ---")
+
+print("\nD().foo(1,2):")
+D().foo(1,2)
+
+print("\nsuper(D,D()).foo(1,2):")
+super(D,D()).foo(1,2)
