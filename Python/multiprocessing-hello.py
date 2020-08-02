0d0862e990b2f7151e224522fc7010130104532e
Andreas Langenhagen <andreas@langenhagen.cc>
2020-08-02 18:35:17 +0200

    Add multiprocessing-hello.py

---
+++ Python/multiprocessing-hello.py
@@ -0,0 +1,26 @@
+#!/usr/env/bin python3
+"""Showcase usage of the module multiprocessing that can create processes for
+multi-core programming.
+
+See: https://www.blog.pythonlibrary.org/2020/07/15/python-101-creating-multiple-processes/
+"""
+import multiprocessing
+import random
+import time
+
+def worker(name: str) -> None:
+    print(f'Started worker {name}')
+    worker_time = random.choice(range(1, 5))
+    time.sleep(worker_time)
+    print(f'{name} worker finished in {worker_time} seconds')
+
+
+if __name__ == '__main__':
+    processes = []
+    for i in range(5):
+        process = multiprocessing.Process(target=worker, args=(f'computer_{i}',))
+        processes.append(process)
+        process.start()
+
+    for proc in processes:
+        proc.join()
