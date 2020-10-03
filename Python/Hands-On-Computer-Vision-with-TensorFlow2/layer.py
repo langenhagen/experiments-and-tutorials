40570a2dfe76aebb0b7b34b09c93b7efc4938195
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-10-04 00:42:16 +0200

    Update neuron and add a simple layer

---
+++ Python/Hands-On-Computer-Vision-with-TensorFlow2/layer.py
@@ -0,0 +1,41 @@
+"""A simple fully connected neural network layer.
+
+author: andreasl
+"""
+import numpy as np
+
+
+class FullyConnectedLayer:
+    """A simple fully connected neural network layer."""
+
+    def __init__(self, n_inputs, layer_size, activation_fn):
+        self.weights = np.random.standard_normal((n_inputs, layer_size))
+        self.bias =  np.random.standard_normal(layer_size)
+        self.size = layer_size
+        self.activation_fn = activation_fn
+
+    def forward(self, x):
+        """Forward the input signal through the layer."""
+        z = np.dot(x, self.weights) + self.bias
+        return self.activation_fn(z)
+
+
+if __name__ == "__main__":
+    np.random.seed(42)
+    x1 = np.random.uniform(-1, 1, 2).reshape(1, 2)
+    print(f"x1: {x1}")
+    x2 = np.random.uniform(-1, 1, 2).reshape(1, 2)
+    print(f"x2: {x2}")
+
+    relu_fn = lambda y: np.maximum(y, 0)
+    layer = FullyConnectedLayer(2, 3, relu_fn)
+
+    out1 = layer.forward(x1)
+    print(f"out1: {out1}")
+    out2 = layer.forward(x2)
+    print(f"out2: {out2}")
+
+    x12 = np.concatenate((x1,x2))
+    print(f"x12: {x12}")
+    out12 = layer.forward(x12)
+    print(f"out12: {out12}")
--- Python/Hands-On-Computer-Vision-with-TensorFlow2/neuron.py
+++ Python/Hands-On-Computer-Vision-with-TensorFlow2/neuron.py
@@ -1,9 +1,7 @@
-"""A simple artificial neuron implementation based on the contents in the book
-Hands-On Computer Vision with TensorFlow 2.
+"""A simple artificial neuron implementation.
 
 author: andreasl
 """
-# /import numpy as np
 import numpy as np
 
 
@@ -11,6 +9,23 @@ class Neuron:
     """A simpe feed-forward artificial Neuron."""
 
     def __init__(self, n_inputs, activation_fn):
-        self.W = np.random.rand(n_inputs)
-        self.b = np.random.rand(1)
+        self.weights = np.random.rand(n_inputs)
+        self.bias = np.random.rand(1)
         self.activation_fn = activation_fn
+
+    def forward(self, x):
+        """Forward the input signal through the neuron."""
+        z = np.dot(x, self.weights) + self.bias
+        return self.activation_fn(z)
+
+
+if __name__ == "__main__":
+    np.random.seed(42)
+    x = np.random.rand(3).reshape(1, 3)
+    print(f"x: {x}")
+    step_fn = lambda y: 0 if y < 0 else 1
+    perceptron = Neuron(x.size, activation_fn=step_fn)
+    print(f"perceptron.weights: {perceptron.weights}")
+    print(f"perceptron.bias: {perceptron.bias}")
+    out = perceptron.forward(x)
+    print(f"out: {out}")
