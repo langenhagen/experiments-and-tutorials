0abbaf2f041c5d2577ec575095e5f48fea37c92e
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-10-29 12:01:01 +0100

    Update sqlalchemy-hello.py

--- Python/sqlalchemy-hello.py
+++ Python/sqlalchemy-hello.py
@@ -1,8 +1,22 @@
 #!/usr/env/bin python
 """Showcase the usage of the external package sqlalchemy to execute queries.
 
-SQL ALchemy has a big documentation. I found the tutorial here
-https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91 concise and helpful.
+SQL ALchemy has a big documentation. I found these tutorials concise and helful:
+- https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91
+- https://www.tutorialspoint.com/sqlalchemy/index.htm
+
+TODO
+- add info "about parameters"
+  https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_core_using_textual_sql.htm
+- aliases
+  https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_core_using_aliases.htm
+- complexer queries
+  https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_core_using_update_expression.htm
+  https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_core_using_conjunctions.htm
+- joins https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_core_using_joins.htm
+- sessions
+  https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_orm_creating_session.htm
+  https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_orm_using_query.htm
 
 Tested with sqlalchemy 1.3.20."""
 import datetime
@@ -11,11 +25,16 @@ import sqlalchemy as db
 
 print("=== 1 - connect to a db ===")
 # engine = db.create_engine("mysql://<user>[:<pass>]@<url>:<port>/<database>")
+# engine = db.create_engine("mysql://root@127.0.0.1:3306/lesemops", echo=True)  # echo=True logs sql statements
 engine = db.create_engine("mysql://root@127.0.0.1:3306/lesemops")
 connection = engine.connect()
 metadata = db.MetaData()
 
-print("\n=== 2 get table information ===")
+print("\n=== 2 get table names ===")
+engine.table_names()
+print(f"engine.table_names(): {engine.table_names()}")
+
+print("\n=== 3 get specific table information ===")
 ankauf_anfragen = db.Table(
     "ankauf_anfragen", metadata, autoload=True, autoload_with=engine
 )
@@ -24,7 +43,12 @@ print(f"str(ankauf_anfragen): {str(ankauf_anfragen)}")
 print(f"repr(ankauf_anfragen): {repr(ankauf_anfragen)}")
 print(f"ankauf_anfragen.columns.keys(): {ankauf_anfragen.columns.keys()}")
 
-print("\n=== 3 - query via sql statements ===")
+print("\n=== 4 - query via sql statements ===")
+
+text = db.text("select * from ankauf_anfragen LIMIT 1000")
+result_proxy = connection.execute(text)
+print(f"type(result_proxy) {type(result_proxy)}")
+# ...or...
 result_proxy = connection.execute("select * from ankauf_anfragen LIMIT 1000")
 print(f"type(result_proxy) {type(result_proxy)}")
 
@@ -32,7 +56,7 @@ result_set = result_proxy.fetchall()
 print(f"type(result_set) {type(result_set)}")
 print(result_set)
 
-print("\n=== 4 - compose a query via functions ===")
+print("\n=== 5 - compose a query via functions ===")
 ankauf_items = db.Table("ankauf_anfragen", metadata, autoload=True, autoload_with=engine)
 query = db.select([ankauf_items]).limit(10)  # select * FROM ankauf_items LIMIT 10
 print(f"str(query): {str(query)}")
@@ -44,7 +68,7 @@ result_set = result_proxy.fetchall()
 print(f"type(result_set) {type(result_set)}")
 print(result_set)
 
-print("\n=== 5 - insert 1 row ===")
+print("\n=== 6 - insert 1 row ===")
 ankauf_brands = db.Table("ankauf_brands", metadata, autoload=True, autoload_with=engine)
 query = db.insert(ankauf_brands).values(
     brand="Mybrand", counter=69, last_search=datetime.datetime.now()
@@ -57,7 +81,7 @@ results = connection.execute(query).fetchall()
 print(f"Results: {results}")
 
 
-print("\n=== 6 insert many rows ===")
+print("\n=== 7 insert many rows ===")
 query = db.insert(ankauf_brands)
 values = [
     {"brand": "my2ndbrand", "counter": 42, "last_search": datetime.datetime.now()},
@@ -69,7 +93,7 @@ print(f"query: {query}")
 results = connection.execute(query).fetchall()
 print(f"Results: {results}")
 
-print("\n=== 7 delete rows ===")
+print("\n=== 8 delete rows ===")
 query = db.delete(ankauf_brands)
 print(f"query: {query}")
 result_proxy = connection.execute(query, values)
