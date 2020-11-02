8c38baa4e1e86b9ab544bd3d2a73e8085d6adcd9
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-11-02 12:12:42 +0100

    Update traceback-hello.py

--- Python/traceback-hello.py
+++ Python/traceback-hello.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 """
-Showcase traceback functionality without Exceptions being involved.
+Showcase traceback functionality without and with Exceptions being involved.
 
 There are different approaches when exceptions are involved.
+
+See: https://docs.python.org/3.6/library/traceback.html
 """
+import sys
 import traceback
+
+
+print("=== 1 print a traceback ===")
 
 
 def foo():
@@ -17,3 +23,24 @@ def bar(i):
     print("Normal traceback END")
 
 foo()
+
+
+print("\n=== 2 get a traceback object from an exception ===")
+
+
+def fail():
+    raise ValueError("Please crash")
+
+
+def catch():
+    try:
+        fail()
+    except ValueError as err:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        formatted = traceback.format_tb(exc_traceback)
+        print(f"type(exc_traceback): {type(exc_traceback)}")
+        print(f"type(formatted): {type(formatted)}")
+        print(f"formatted:\n {formatted}")
+
+
+catch()
