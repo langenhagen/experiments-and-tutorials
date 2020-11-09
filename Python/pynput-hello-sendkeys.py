bf8ebfa4684758c498cb0062039943b12b451698
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-11-09 23:23:43 +0100

    Add pynput-hello-sendkeys.py

---
+++ Python/pynput-hello-sendkeys.py
@@ -0,0 +1,22 @@
+#!/usr/env/bin python
+"""Showcase the sending keys via the 3rd party package pynput
+
+Tested with pynput 1.7.1.
+
+Taken from: https://nitratine.net/blog/post/simulate-keypresses-in-python/
+
+author: andreasl
+"""
+import time
+import random
+from pynput.keyboard import Controller
+
+keyboard = Controller()  # Create the controller
+
+def type_string_with_delay(string):
+    for character in string:  # Loop over each character in the string
+        keyboard.type(character)  # Type the character
+        delay = random.uniform(0, 2)  # Generate a random number between 0 and 10
+        time.sleep(delay)  # Sleep for the amount of seconds generated
+
+type_string_with_delay("This is my string typed with a delay")
