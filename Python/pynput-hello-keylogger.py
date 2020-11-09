a5f439559576fcf90651a31fd70c49f186c12ed4
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-11-09 23:18:59 +0100

    Update, fix and rename pynput-hello-keylogger.py

--- Python/pynput-hello-keylogger.py
+++ Python/pynput-hello-keylogger.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python3
-"""Showcase third-party library pynput
+#!/usr/bin/env python
+"""Showcase how to catch keyboard input with the third-party library pynput
 and how to use it as a key logger.
+
+The window doesn't even need focus to grab the keyboard input.
 
 See:
 https://pythonhosted.org/pynput/keyboard.html#monitoring-the-keyboard
 https://www.youtube.com/watch?v=TbMKwl11itQ
+
+Tested with pynput 1.7.1.
+
+author: andreasl
 """
 import pynput
 
@@ -15,13 +21,13 @@ def on_press(key):
     """Do some action on keyboard key press."""
     print(f"Key {key} pressed")
     if key is pynput.keyboard.Key.enter:
-        print("Key is also enter!")
+        print("Moreover, key is also enter!")
 
 
 def on_release(key):
     """Do some action on keyboard key release."""
     print(f"Key {key} released")
-    if key == pynput.Key.esc:
+    if key == pynput.keyboard.Key.esc:
         # Stop listener
         return False
 
