da519f1db64755c01bb9579ebe10d2c15d440171
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-11-21 00:21:28 +0100

    Add pandas-outliers-hello.py

---
+++ Python/pandas-hello/pandas-outliers-hello.py
@@ -0,0 +1,16 @@
+#!/usr/env/bin pandas
+"""Showcase how to filter outliers from a dataframe."""
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+df = pd.DataFrame(np.random.randn(100, 3))
+print(df)
+
+print("=== 1 use a z-score via scipy to filter outliers ===")
+
+print(stats.zscore(df))
+
+# filtered = df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]  # z-score < 3 is typical for filtering
+filtered = df[(np.abs(stats.zscore(df)) < 2).all(axis=1)]  # smaller z-score filters more
+print(filtered)
