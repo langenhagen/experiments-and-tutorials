f6db9da4c78538914bea0508bb3cf2387c293c52
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-12-29 13:27:37 +0100

    Add exception-raise-from-hello.py

---
+++ Python/exception-raise-from-hello.py
@@ -0,0 +1,8 @@
+#!/usr/env/bin/python
+"""Showcase the usage of raise ... from to raise sequences of exceptions."""
+
+try:
+    raise ValueError("The first message")
+except ValueError as err:
+    # following raise logs both the first exception and the second exception
+    raise KeyError("Yep, the second message") from err
