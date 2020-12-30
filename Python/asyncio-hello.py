430d1bee147cb6a8b2948c82fba0a645c9b5a9a1
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-12-30 15:51:23 +0100

    Add asyncio-hello.py

---
+++ Python/asyncio-hello.py
@@ -0,0 +1,37 @@
+#!/usr/env/bin python
+"""Showcase the usage of the python standard library `asyncio`.
+
+I don't really get the benefits yet but I also scratched part of it.
+
+See docs: https://docs.python.org/3/library/asyncio-task.html
+"""
+import asyncio
+import time
+
+print("--- 1 simple call to asyncio.run() ---")
+
+async def foo():
+    print("Hello from foo!")
+    await asyncio.sleep(1)
+    print("Goodbye from foo!")
+
+print("Go program")
+asyncio.run(foo())
+print("Goodbye program")
+
+
+print("--- 2 nested await ---")
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def foo():
+    print(f"started at {time.strftime('%X')}")
+
+    await say_after(2, 'hello')
+    await say_after(1, 'world')
+
+    print(f"finished at {time.strftime('%X')}")
+
+asyncio.run(foo())
