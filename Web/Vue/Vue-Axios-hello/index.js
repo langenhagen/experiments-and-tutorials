2c401b0d9d3acec2f2784c080cd7b9c4c06f99a1
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-01-04 23:07:20 +0100

    Avoid flickering with vue
    And apparently more.

--- Web/Vue/Vue-Axios-hello/index.js
+++ Web/Vue/Vue-Axios-hello/index.js
@@ -1,13 +1,21 @@
 new Vue({
-  el: '#app',
+  el: "#app",
   data () {
     return {
-      info: null
+      info: ""
     }
   },
   mounted () {
     axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = response))
-  }
+      .post("http://localhost:5000/api", {
+        query: '{ expense(id:"074d6f32-8072-400a-b84e-b95b65accd21") { amount employee { firstName lastName } } }'
+      })
+      .then(response => (this.info = response.data.data))
+      .catch(error => console.log(`Thats an error: ${error}`))
+    }
 })
+
+
+// '{ "query": "{ expense(id:"074d6f32-8072-400a-b84e-b95b65accd21") { amount employee { firstName lastName } } }" }'
+
+// curl -X POST -H "Content-Type: application/json" --data '{ "query": "{ expense(id:\"074d6f32-8072-400a-b84e-b95b65accd21\") { amount employee { firstName lastName } } }" }' http://localhost:5000/api
