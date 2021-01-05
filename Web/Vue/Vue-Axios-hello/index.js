dfbb847f885f57cad89e2d619ae152f4a438e1f8
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-01-05 01:23:46 +0100

    Update Vue-Axios-hello

--- Web/Vue/Vue-Axios-hello/index.js
+++ Web/Vue/Vue-Axios-hello/index.js
@@ -1,20 +1,90 @@
+function on_response(response) {
+  this.expenses = response.data.data.expenses
+  // alert(`Thats a problem:\n\n${JSON.stringify(response.data.data.expenses)}`)
+}
+
+function on_error(error) {
+  console.log(`Thats an error: ${error}`)
+  alert(`Thee error: ${error}`)
+}
+
 new Vue({
   el: "#app",
-  data () {
+  data() {
     return {
-      info: ""
+      expenses: "",
     }
   },
-  mounted () {
+  mounted() {
     axios
       .post("http://localhost:5000/api", {
-        query: '{ expense(id:"074d6f32-8072-400a-b84e-b95b65accd21") { amount employee { firstName lastName } } }'
+        // query: `{ expense(id:"684a1482-ce20-4e74-8df2-0bf4af89fd09") { amount employee { firstName lastName } } }`
+        query: `
+               {
+                 expenses {
+                   id
+                   description
+                   createdAt
+                   amount
+                   currency
+                   state
+                   employee {
+                     id
+                     firstName
+                     lastName
+                   }
+                 }
+               }
+               `
       })
-      .then(response => (this.info = response.data.data))
-      .catch(error => console.log(`Thats an error: ${error}`))
-    }
+      .then(on_response.bind(this))
+      .catch(on_response.bind(this))
+  }
 })
 
+
+
+    // axios.post({
+    //   url: "http://localhost:5000/api",
+    //   data: {
+    //     query: `
+    //       {
+    //         expenses {
+    //           id
+    //           employee {
+    //             firstName
+    //             lastName
+    //           }
+    //         }
+    //       }
+    //       `
+    //   }
+    // })
+//       .then(this.on_received)
+//       .catch(this.on_error)
+//   }
+// })
+
+
+
+// axios({
+//   url: "http://localhost:5000/api",
+//   method: "post",
+//   data: {
+//     query: `
+//       query expenses {
+//         id
+//         employee {
+//           firstName
+//           lastName
+//             }
+//           }
+//         }
+//       `
+//   }
+// }).then((result) => {
+//   console.log(result.data)
+// });
 
 // '{ "query": "{ expense(id:"074d6f32-8072-400a-b84e-b95b65accd21") { amount employee { firstName lastName } } }" }'
 
