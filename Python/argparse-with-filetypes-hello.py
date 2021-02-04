f828b5d9d893218fba8d90bc940987bd8d026bc4
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-02-04 15:51:06 +0100

    Format argparse-with-filetypes-hello.py
    Format the file `argparse-with-filetypes-hello.py` nicely with the
    Python formatter `black`.

--- Python/argparse-with-filetypes-hello.py
+++ Python/argparse-with-filetypes-hello.py
@@ -4,20 +4,12 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    '-i',
-    metavar='in-file',
-    type=argparse.FileType('rt')
-)
-parser.add_argument(
-    '-o',
-    metavar='out-file',
-    type=argparse.FileType('wt')
-)
+parser.add_argument("-i", metavar="in-file", type=argparse.FileType("rt"))
+parser.add_argument("-o", metavar="out-file", type=argparse.FileType("wt"))
 
 try:
     results = parser.parse_args()
-    print('Input file:', results.i)
-    print('Output file:', results.o)
+    print("Input file:", results.i)
+    print("Output file:", results.o)
 except IOError as msg:
     parser.error(str(msg))
