92f104b30de9981256b2ccbac454eed3ca92a9f9
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-02-16 00:53:28 +0100

    Update yfinance-hello.py

--- Python/yfinance-hello/yfinance-hello.py
+++ Python/yfinance-hello/yfinance-hello.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 """Showcase usage of the 3rd party package `yfinance`."""
+import datetime as dt
+
+import forex_python.converter
 import yfinance as yf
 
 # define the ticker symbol
-tickerSymbol = "SWDA.SW"
+ticker_symbol = "SWDA.SW"
 
 # get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
+ticker_data = yf.Ticker(ticker_symbol)
 
 # get the historical prices for this ticker
-tickerDf = tickerData.history(period="1d", start="2010-1-1", end="2052-1-25")
+df = ticker_data.history(period="1d", start="2010-1-1", end="2052-1-25")
 
 # see your data
-print(tickerDf)
+rate_converter = forex_python.converter.CurrencyRates()
+print(df * rate_converter.get_rate("USD","EUR", dt.datetime(2021,2,12)))
