dec014dd7ed93b19171ca0dc14f4153bd211d812
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-02-17 16:13:22 +0100

    Add base64-hello.py

---
+++ Python/base64-hello.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""Showcase the standard library package `base64` to convert data from and to
+base64-encoded strings."""
+import base64
+
+print("--- 1 - encode strings ---")
+
+msg = "Be excellent\nto each other"
+msg_bytes = msg.encode("ascii")
+base64_bytes = base64.b64encode(msg_bytes)
+base64_msg = base64_bytes.decode("ascii")
+
+print(base64_msg)
+
+print("--- 2 - decode strings ---")
+
+base64_bytes = base64_msg.encode("ascii")
+msg_bytes = base64.b64decode(base64_bytes)
+msg = msg_bytes.decode("ascii")
+
+print(msg)
