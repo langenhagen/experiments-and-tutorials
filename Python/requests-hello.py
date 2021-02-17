3f14ca571bb8a6a0e9236d38161fad6421b9975e
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-02-17 13:52:02 +0100

    Add info to requests-hello.py

--- Python/requests-hello.py
+++ Python/requests-hello.py
@@ -5,12 +5,14 @@ import requests
 
 print("--- 1 - GETting stuff ---")
 r = requests.get("https://google.com")
+r.raise_for_status()  # throws an exception if the response doesn't have a 2xx success error code
 print(r)  # 200
 print(r.headers)
 print(r.content)
 
 print("--- 2 - POSTing stuff ---")
 r = requests.post("https://httpbin.org/post", data={"key": "value"})
+r.raise_for_status()
 
 print(f"Type: {type(r.json())}")
 assert isinstance(r.json(), dict)
