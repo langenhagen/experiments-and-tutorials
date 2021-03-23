6c8b5c3e2e8d0edb9a294b69e3bb0d9003185c4d
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-03-23 16:58:39 +0100

    Add some Go experiments

---
+++ Go/defer.go
@@ -0,0 +1,19 @@
+// A defer statement defers the execution of a function until the surrounding function returns.
+//
+// The deferred call's arguments are evaluated immediately, but the function call is not executed
+// until the surrounding function returns.
+//
+// taken from: https://tour.golang.org/flowcontrol/12
+//
+// author: andreasl
+package main
+
+import "fmt"
+
+// Prints `hello` then `nice` then `world`.
+func main() {
+	defer fmt.Println("world")
+	defer fmt.Println("nice")
+
+	fmt.Println("hello")
+}
---
+++ Go/variables-with-initializers.go
@@ -0,0 +1,16 @@
+// Taken from https://tour.golang.org/basics/9.
+// author: andreasl
+// run via:
+//   go run variables-with-initializers.go
+
+
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java = true, false, "no!"  // works!
+	fmt.Println(i, j, c, python, java)
+}
