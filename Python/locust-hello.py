0130ee8f8ff153282cc2850f2b08f3eda8fc3509
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-08-06 16:59:58 +0200

    Add locust-hello.py

---
+++ Python/locust-hello.py
@@ -0,0 +1,24 @@
+"""
+Experimental locust https://locust.io/ test.
+
+Apparently, locust is a neat thing to load-test response times including graphs
+and stuff.
+
+Install locust via:
+
+    pip install locust
+
+
+based on: https://www.youtube.com/watch?v=SOu6hgklQRA
+
+author: andreasl
+"""
+from locust import HttpUser, task, between
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def live_stock(self):
+        self.client.get(url="/storage/live_stock")
