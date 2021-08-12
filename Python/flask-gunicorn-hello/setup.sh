cc89730d426d04ea0e51ed5690dfb1731ca4437f
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-08-12 17:10:17 +0200

    Update Python/flask-gunicorn-hello/setup.sh

--- Python/flask-gunicorn-hello/setup.sh
+++ Python/flask-gunicorn-hello/setup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-# Set up the project for local development.
+# Set up the Python project for local development.
 # Set up a Python virtual environment and install the dependencies.
-# Also, when --dev is specified, set up an npm project to install npm dev dependencies.
 #
 # Usage:
-#   scripts/setup.sh         # setup the project
+#
+#   scripts/setup.sh [--clean]
+#
+# Examples:
+#
+#   scripts/setup.sh               # set up the project
+#   scripts/setup.sh --clean       # clean already existing artifacts and set up the project
 set -e
 
-rm -fr .venv/
-python -m venv .venv
+[[ "$*" =~ '--clean' ]] && { rm -fr .venv/; python -m venv .venv; }
 
 # shellcheck disable=SC1091
 source .venv/bin/activate
 
 pip install --upgrade pip
 pip install --upgrade -r requirements.txt
+
