5f96b55fe418a2d177f6a37f8497ea0180d9ce29
Andreas Langenhagen <andreas.langenhagen@tier.app>
2021-09-16 17:18:41 +0200

    Add dis-hello.py

---
+++ Python/dis-hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Showcase the usage od th python bytecode disassembly package `dis` in the
+standard library."""
+import dis
+
+
+print("--- 1 - simple disassembly ---")
+
+
+def foo(i: int):
+    j = i + 42
+    return j
+
+
+v = foo(23)
+print(f"Result of foo(23) = {v}")
+
+print("\nThe result of dis.dis(foo):\n")
+dis.dis(foo)
