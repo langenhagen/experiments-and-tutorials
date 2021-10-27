89e7a7cdc13cbc3ea433c951b9a47cd036c0a3bb
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-10-27 14:21:00 +0200

    Add falsiness-hello.py
    Mostly to see what linters say about the differences.

---
+++ Python/falsiness-hello.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Showcase the usage of boolean checks."""
+
+print("=== 1 falsiness of False ===")
+
+cond = False
+
+if not cond:  # triggers
+    print("not cond")
+
+if cond is False:  # triggers
+    print("cond is False")
+
+print("=== 1 falsiness of None ===")
+
+cond2 = None
+
+if not cond2:  # triggers
+
+    print("not cond")
+
+if cond2 is False:  # doesn't trigger
+    print("cond is False")
+
+# not recommended by flake8 and pylint
+# comparison to False should be 'if cond is False:' or 'if not cond:'
+# if cond == False:
+#     print("cond == False")
