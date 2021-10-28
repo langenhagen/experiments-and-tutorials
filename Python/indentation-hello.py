0e1477d9751e4aa451c5076319aa76a6e133ba13
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-10-28 12:02:04 +0200

    Add indentation-hello.py

---
+++ Python/indentation-hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Showcase that working with different indentations in functions works."""
+
+
+def normal_indented():
+    print("This has a 4 characters indentation")
+
+
+def three_spaces_indented():
+   print("This has a 3 characters indentation")
+
+
+normal_indented()
+three_spaces_indented()
