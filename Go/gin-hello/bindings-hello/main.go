2af03094fa472cab261deb0373573507a52ace93
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-12-06 15:11:18 +0100

    Add gin-hello/bindings-hello

---
+++ Go/gin-hello/bindings-hello/main.go
@@ -0,0 +1,55 @@
+// Showcase the `binding` feature from gin. based on:
+// https://github.com/gin-gonic/gin#model-binding-and-validation
+//
+// Uage examples:
+//
+// 1. curl --data '{"user":"andi", "pass":"123"}' -X POST 'localhost:8080/login'
+//
+//    results: {"status":"you are logged in"}
+//
+// 2. curl --data '{"user":"carl"}' -X POST 'localhost:8080/login'
+//
+//    results: {"status":"unauthorized"}
+//
+// 3. curl --data '{"pass":"123"}' -X POST 'localhost:8080/login'
+//
+//   results: {"error":"Key: 'Login.User' Error:Field validation for 'User' failed on the 'required' tag"}
+//
+// Binding seems to be even more capable. See:
+// https://blog.logrocket.com/gin-binding-in-go-a-tutorial-with-examples/
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Binding from JSON
+type Login struct {
+	User string `json:"user" binding:"required"`
+	// binding:"-" seems to be like not writing it at all
+	// see: https://github.com/gin-gonic/gin#:~:text=the%20%27required%27%20tag%22%7D-,Skip%20validate,-When%20running%20the
+	Password string `json:"pass" binding:"-"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/login", func(c *gin.Context) {
+		var json Login
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if json.User != "andi" || json.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+
+	router.Run(":8080")
+}
