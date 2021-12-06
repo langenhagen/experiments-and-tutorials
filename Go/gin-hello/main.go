667adfcb21b0a545622f5404845e950fbaba1480
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-12-06 11:08:42 +0100

    Add Go/gin-hello

---
+++ Go/gin-hello/main.go
@@ -0,0 +1,15 @@
+// Gin-hello showcases the usage of the web framework `gin`.
+// See: https://github.com/gin-gonic/gin#quick-start
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
