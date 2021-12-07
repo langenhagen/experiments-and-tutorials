e0d0911f6c829e0d5839a1b32b6e0c6d65be7738
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-12-07 17:50:27 +0100

    Add structs-hello.go

---
+++ Go/structs-hello.go
@@ -0,0 +1,18 @@
+// Showcase structs in go.
+package main
+
+import "fmt"
+
+type MyStruct struct {
+	Hello int
+	World string
+}
+
+func main() {
+	var o MyStruct
+	o = MyStruct{} // initializes an object with Hello=0 and World=""
+	fmt.Printf("MyStruct{}: %+v\n", o)
+
+	o = MyStruct{World: "DisneyWorld"} // initializes an object with Hello=0
+	fmt.Printf("MyStruct{World:\"DisneyWorld\": %+v\n", o)
+}
