471b97f7029265f17cd814d078e293e91f365cd0
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-12-22 16:03:17 +0100

    Add Go/gjson-hello

---
+++ Go/gjson-hello/go.mod
@@ -0,0 +1,14 @@
+module gjson-hello
+
+go 1.17
+
+require (
+	github.com/satori/go.uuid v1.2.0
+	github.com/tidwall/gjson v1.12.1
+)
+
+require (
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
---
+++ Go/gjson-hello/main.go
@@ -0,0 +1,44 @@
+// Showcase the usage of the 3rd party library `gjson`
+package main
+
+import (
+	"fmt"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/tidwall/gjson"
+)
+
+// "github.com/google/uuid" // unmarshalling UUIDs works also with google/uuid
+
+func jsonStringToGjson() {
+	s := `
+		{
+			"hi": "there",
+			"answer": 42,
+			"values": [1, 2, 3],
+			"nested": {
+				"name": "Derp",
+				"lastname": "Derpsen"
+			},
+			"id": "11111111-1111-4111-8111-111111111111"
+		}`
+	fmt.Printf("string: %s\n", s)
+
+	r := gjson.Parse(s)
+	fmt.Printf("gjson result: %v\n", r)
+	fmt.Printf("hi: %v\n", r.Get("hi"))
+	fmt.Printf("anwers: %v\n", r.Get("answer"))
+	fmt.Printf("values is an array: %v\n", r.Get("values").IsArray())
+	fmt.Printf("values: %v\n", r.Get("values"))
+	fmt.Printf("nested.name: %v\n", r.Get("nested.name").String())
+	id, e := uuid.FromString(r.Get("id").String())
+	if e != nil {
+		panic(e)
+	}
+	fmt.Printf("id: %v\n", id)
+}
+
+func main() {
+	fmt.Println("--- 1 json string - gjson ---")
+	jsonStringToGjson()
+}
