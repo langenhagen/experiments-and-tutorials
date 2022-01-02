10449d8f0892302bab0942a6a3c12c76219b3724
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-01-02 23:05:00 +0100

    Add enums-hello.py

---
+++ Python/enums-hello.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Showcase the usage of Python enumerations."""
+import enum
+
+print("--- 1 simple enums ---")
+
+
+class MyEnum:
+    """Simple enum with enum.auto()"""
+
+    VAL_1 = enum.auto()
+    VAL_2 = enum.auto()
+
+
+a = MyEnum.VAL_1
+b = MyEnum.VAL_2
+
+print(f"{a=}")
+print(f"{b=}")
+
+
+print("--- 2 enums with string values ---")
+
+
+class MyStringEnum:
+    """Enum with string values."""
+
+    VAL_1 = "my val"
+    VAL_2 = "my other val"
+
+
+c = MyStringEnum.VAL_1
+d = MyStringEnum.VAL_2
+
+print(f"{c=}")
+print(f"{d=}")
