57bea95945ed4d326fad86fe5ac0bf8473070991
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-03-23 11:02:37 +0100

    Add newlines to requests-hello.py

--- Python/requests-hello.py
+++ Python/requests-hello.py
@@ -4,14 +4,14 @@ import json
 
 import requests
 
-print("--- 1 - GETting stuff ---")
+print("--- 1 - GETting stuff ---\n")
 r = requests.get("https://google.com")
 r.raise_for_status()  # throws an exception if the response doesn't have a 2xx success error code
 print(r)  # 200
 print(r.headers)
 print(r.content)  # in bytes; use r.text for str content
 
-print("--- 2 - POSTing stuff ---")
+print("\n--- 2 - POSTing stuff ---\n")
 r = requests.post("https://httpbin.org/post", json={"key": "value"})
 r.raise_for_status()
 
@@ -19,5 +19,5 @@ print(f"Type: {type(r.json())}")
 assert isinstance(r.json(), dict)
 print(json.dumps(r.json()))
 
-print("--- 3 - TODO sessions ---")
+print("\n--- 3 - TODO sessions ---\n")
 s = requests.Session()
