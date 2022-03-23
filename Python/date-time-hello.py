b48ed3e51c10fc8f2c6787933fbe6fdfd3fff506
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-03-23 11:02:45 +0100

    Extend date-time-hello.py

--- Python/date-time-hello.py
+++ Python/date-time-hello.py
@@ -2,16 +2,28 @@
 """Showcase the use of date and time in Python."""
 import datetime as dt
 
-print("--- 1 - now ---")
+print("--- 1 - now ---\n")
 now = dt.datetime.now()
 utcnow = now.utcnow()
+utcnow2 = dt.datetime.now(tz=dt.timezone.utc)
 
 print(f"{now=}")
 print(f"{utcnow=}")
 
-print("--- 2 - timezones ---")
+print("\n--- 2 - timezones ---\n")
 print(f"{now.utcoffset()=}")
 print(f"{utcnow.utcoffset()=}")
+print(f"{utcnow2.utcoffset()=}")
 
-print("--- 3 - datetime differences ---")
-print(f"{now - utcnow=}")  # both now and utcnow have no timezone
+print("\n--- 3 - datetime differences ---\n")
+# if your machine is not in UTC, there will be a difference since both now and
+# utcnow have no timezone
+print(f"{now - utcnow=}")
+
+try:
+    print(f"{utcnow - utcnow2=}")
+except TypeError:  # can't subtract offset-naive and offset-aware datetimes
+    print(
+        "Exception: can't subtract offset-naive and offset-aware datetimes"
+        f"{utcnow=} and {utcnow2=}"
+    )
