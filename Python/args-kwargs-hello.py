b0a7b2abcf443f3b5d67765e9b1c148b7fab325d
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-04-04 21:36:31 +0200

    Add args-kwargs-hello.py

---
+++ Python/args-kwargs-hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Showcase the usage of args and kwargs."""
+
+
+def my_purely_kwargs_function(**kwargs):
+    """Showcase the implementation of a pure kwargs function."""
+    print(f"{kwargs=}")
+    print(f"{type(kwargs)=}")
+
+
+print("\n--- 1 pure kwargs ---\n")
+
+print("Without kwargs")
+my_purely_kwargs_function()
+
+print("With kwargs")
+my_purely_kwargs_function(i=23, n="Jorge")
