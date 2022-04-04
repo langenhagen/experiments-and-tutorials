bfe4983039aad5792dee605e49d41041bdd7bee6
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-04-04 16:42:27 +0200

    Extend, improve and reformat argparse-hello.py

--- Python/argparse-hello.py
+++ Python/argparse-hello.py
@@ -6,23 +6,34 @@ given, it uses sys.argv[1:] by default.
 See:
 https://pymotw.com/3/argparse/index.html
 https://docs.python.org/3/library/argparse.html#nargs
+https://stackoverflow.com/questions/15753701/how-can-i-pass-a-list-as-a-command-line-argument-with-argparse
 """
 import argparse
 import pathlib
+from argparse import Namespace
 
 parser = argparse.ArgumentParser(description="Short sample app")
 
 parser.add_argument(
-    "-a", action="store_true", default=False, help='help string for "a"'
+    "-a",
+    action="store_true",
+    default=False,
+    help='help string for "a"',
 )
-parser.add_argument("-b", action="store", dest="b", help='help string for "b"')
+parser.add_argument(
+    "-b",
+    action="store",
+    dest="b",
+    help='help string for "b"',
+)
 parser.add_argument(
     "-c",
     "--alternative-C",
     action="store",
     dest="c",
     type=int,
-    help='help string for "c"; very long help strings appear to be broken into multiple lines at runtime, but\nignore newlines\n',
+    help='help string for "c"; very long help strings appear to be broken '
+    "into multiple lines at runtime, but\nignore newlines\n",
 )
 parser.add_argument("-d", action="store", dest="d", help='help string for "d"')
 parser.add_argument(
@@ -42,78 +53,82 @@ parser.add_argument(
     help="an unnamed optional argument",
 )
 parser.add_argument(
-    "my_mandatory_positional_arg",
+    "my_required_positional_arg",
     # nargs=1,  # 1: list of 1 item
-    help="an unnamed mandatory argument",
+    help="a required unnamed argument",
+)
+parser.add_argument(
+    "-l",
+    "--my-list",
+    nargs="+",  # `+` takes 1 or more args, `*` takes 0 or more args
+    required=False,  # False is the default, but for showcasing, I write it out
+    help="an optional argument list",
+)
+parser.add_argument(
+    "-m", "--my-other-list", action="append", help="another optional argument list"
 )
 
-print("---1---")
-# namespace = parser.parse_args()  # use parse_args() without params to parse from the command line
-namespace = parser.parse_args(["-a", "-bval", "-c", "3", "-d", "moo", "miaaauuu"])
-print(namespace)
-print(f"a: {namespace.a}")
-print(f"b: {namespace.b}")
-print(f"c: {namespace.d}")
-print(f"noarg: {namespace.noarg}")
-print(f"my_optional_positional_arg: {namespace.my_optional_positional_arg}")
-print(f"my_mandatory_positional_arg: {namespace.my_mandatory_positional_arg}")
 
-print("---2---")
+def __print_namespace(ns: Namespace):
+    """A helper function to print a name space."""
+    print(ns)
+    print(f"{ns.a=}")
+    print(f"{ns.b=}")
+    print(f"{ns.c=}")
+    print(f"{ns.d=}")
+    print(f"{ns.noarg=}")
+    print(f"{ns.my_optional_positional_arg=}")
+    print(f"{ns.my_required_positional_arg=}")
+    print(f"{ns.my_list=}")
+    print(f"{ns.my_other_list=}")
+
+
+print("\n---1 argument parsing ---\n")
+# use parse_args() without params to parse from the command line
+# namespace = parser.parse_args()
+
+# fmt: off
+namespace = parser.parse_args(
+    [
+        "-a",
+        "-bval",
+        "-c", "3",
+        "-d", "moo",
+        "miaaauuu",
+        "--my-list", "1", "2", "3",
+        "-m", "7", "-m", "8", "-m", "9",
+    ]
+)
+# fmt: on
+__print_namespace(namespace)
+
+print("\n---2 another parsing---\n")
 namespace = parser.parse_args(["-a", "--noarg", "kaaatz"])
-print(namespace)
-print(f"a: {namespace.a}")
-print(f"b: {namespace.b}")
-print(f"c: {namespace.d}")
-print(f"noarg: {namespace.noarg}")
-print(f"my_optional_positional_arg: {namespace.my_optional_positional_arg}")
-print(f"my_mandatory_positional_arg: {namespace.my_mandatory_positional_arg}")
+__print_namespace(namespace)
 
-print("---3---")
+print("\n---3 with only one argument---\n")
 namespace = parser.parse_args(["just one argument works"])
-print(namespace)
-print(f"a: {namespace.a}")
-print(f"b: {namespace.b}")
-print(f"c: {namespace.d}")
-print(f"noarg: {namespace.noarg}")
-print(f"my_optional_positional_arg: {namespace.my_optional_positional_arg}")
-print(f"my_mandatory_positional_arg: {namespace.my_mandatory_positional_arg}")
+__print_namespace(namespace)
 
-print("---4---")
-namespace = parser.parse_args(["optional", "mandatory"])  # order is important
-print(namespace)
-print(f"a: {namespace.a}")
-print(f"b: {namespace.b}")
-print(f"c: {namespace.d}")
-print(f"noarg: {namespace.noarg}")
-print(f"my_optional_positional_arg: {namespace.my_optional_positional_arg}")
-print(f"my_mandatory_positional_arg: {namespace.my_mandatory_positional_arg}")
+print("\n---4 yet another parsiong---\n")
+namespace = parser.parse_args(["optional", "required"])  # order is important
+__print_namespace(namespace)
 
-print("---5---")
-# namespace = parser.parse_args([])  # would break sinde my_mandatory_positional_arg is mandatory
-# print(namespace)
-# print(f"a: {namespace.a}")
-# print(f"b: {namespace.b}")
-# print(f"c: {namespace.d}")
-# print(f"noarg: {namespace.noarg}")
-# print(f"my_optional_positional_arg: {namespace.my_optional_positional_arg}")
-# print(f"my_mandatory_positional_arg: {namespace.my_mandatory_positional_arg}")
+print("\n---5 empty argument list might break ---\n")
+# breaks since my_required_positional_arg is required
+# namespace = parser.parse_args([])
 
-print("---6---")
-# namespace = parser.parse_args(['many', 'arguments', 'don\'t', 'due to nargs'])  # error: unrecognized arguments
+print("\n---6 crash with unrecognized positional arguments ---\n")
+# error: unrecognized arguments
+# namespace = parser.parse_args(["many", "arguments", "don't", "due to nargs"])
 
-print("---7---")
-# namespace = parser.parse_args(['-x', 42])  # crashes: TypeError: 'int' object is not subscriptable
+print("\n---7 crash with unknown named arguments ---\n")
+# namespace = parser.parse_args(["-x", "42"])
 
-print("---8---")
-namespace = parser.parse_args(["--help"])  # prints the help and then exits
-# namespace = parser.parse_args(['-h'])  # prints the help and then exits
+print("\n---8 print help and exit ---\n")
+# namespace = parser.parse_args(["--help"])  # prints the help and then exits
+# namespace = parser.parse_args(["-h"])  # prints the help and then exits
 
-print("---9---")
-# namespace = parser.parse_args(['--version']) # prints the program name with the version and then exits
-# print(namespace)
-# print(f"a: {namespace.a}")
-# print(f"b: {namespace.b}")
-# print(f"c: {namespace.d}")
-# print(f"noarg: {namespace.noarg}")
-# print(f"my_optional_positional_arg: {namespace.my_optional_positional_arg}")
-# print(f"my_mandatory_positional_arg: {namespace.my_mandatory_positional_arg}")
+print("\n---9 print version and exit ---\n")
+# namespace = parser.parse_args(["--version"])  # prints the version and then exits
+# namespace = parser.parse_args(["-v"])  # prints the version and then exits
