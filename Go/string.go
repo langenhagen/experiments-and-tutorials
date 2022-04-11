bd5537e69b825aec3b60db1f7058dbbdda47308f
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-04-11 23:23:50 +0200

    Extend and improve string.go

--- Go/string.go
+++ Go/string.go
@@ -6,12 +6,29 @@ import (
 	"strings"
 )
 
-func main() {
-	fmt.Println("--- 1 HasSuffix ---")
+func hasSuffix() {
 	s := "Hello, World!"
 	fmt.Printf("s:   %v\n", s)
 	suffix := "World"
 	fmt.Printf("Has suffix \"%v\":   %v\n", suffix, strings.HasSuffix(s, suffix))
 	suffix = "World!"
 	fmt.Printf("Has suffix \"%v\":   %v\n", suffix, strings.HasSuffix(s, suffix))
+}
+
+func convertByteSlicesToAndFromString() {
+	s := `Ima string`
+	b := []byte(s)
+	t := string(b)
+
+	fmt.Printf("String s: %s\n", s)
+	fmt.Printf("[]byte: %+v\n", b)
+	fmt.Printf("String t: %s\n", t)
+}
+
+func main() {
+	fmt.Println("--- 1 HasSuffix ---")
+	hasSuffix()
+
+	fmt.Println("\n--- 2 byte[] and string conversion is trivial ---")
+	convertByteSlicesToAndFromString()
 }
