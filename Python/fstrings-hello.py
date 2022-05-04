0d522b16dc977e1a4fc8523034538f9c21274032
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-05-04 14:37:46 +0200

    Improve and extend fstrings-hello.py

--- Python/fstrings-hello.py
+++ Python/fstrings-hello.py
@@ -1,25 +1,29 @@
 #!/usr/env/bin python3
 """Showcas the f-strings that exist since Python 3.6."""
 
-print("--- 1 f strings bind early, just like normal ---")
+print("--- 1 f-strings bind early, just like normal ---")
 
-v = 23
+i = 23
 
-a = "for a %% formatted string, v is %d" % v  # pylint complains: C0209 Formatting a regular string which could be a f-string
-b = f"for an f-string, v is {v}"
+# pylint complains: C0209 Formatting a regular string which could be a f-string
+a = "for a %% formatted string, i is %d" % i
+b = f"for an f-string, i is {i}"
 
-print(a)  # for a % formatted string, v is 23
-print(b)  # for an f-string, v is 23
+print(a)  # for a % formatted string, i is 23
+print(b)  # for an f-string, i is 23
 
-v = 42
+i = 42
 
-print(a)  # for a % formatted string, v is 23
-print(b)  # for an f-string, v is 23
+print(a)  # for a % formatted string, i is 23
+print(b)  # for an f-string, i is 23
 
-print("--- 2 f strings have sugar ---")
+print("\n--- 2 f-strings have sugar ---")
 
+i = 42
 f = 1.234
 
-print(f"Use of equals sign: {f=}")
-print(f"Use of equals sign with arbitrary code: {1+2+3=}")
-print(f"Use of equals sign: {f:.2f}")
+print(f"Use of equals sign {{f=}}: {f=}")
+print(f"Use of equals sign with arbitrary code {{1+2+3=}}: {1+2+3=}")
+print(f"Round number {{f:.2f}}: {f:.2f}")
+print(f"Whitespace-pad numbers {{f:10f}}: {f:10f}")
+print(f"Zero-pad integers {{i:x0d}}: {i:03d}")
