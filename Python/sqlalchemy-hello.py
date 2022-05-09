1b9567acebc11bd8c5d9c02800184ab157d85189
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-05-09 14:58:57 +0200

    Align and simplify `sqlalchemy-hello.py`

--- Python/sqlalchemy-hello.py
+++ Python/sqlalchemy-hello.py
@@ -28,67 +28,67 @@ import datetime
 
 import sqlalchemy as db
 
-print("=== 1 - connect to a db ===")
+print("--- 1 - connect to a db ---")
 # engine = db.create_engine("mysql://<user>[:<pass>]@<url>:<port>/<database>")
 # engine = db.create_engine("mysql://root@127.0.0.1:3306/lesemops", echo=True)  # echo=True logs sql statements
 engine = db.create_engine("mysql://root@127.0.0.1:3306/lesemops")
 connection = engine.connect()
 metadata = db.MetaData()
 
-print("\n=== 2 get table names ===")
+print("\n--- 2 get table names ---")
 engine.table_names()
 print(f"engine.table_names(): {engine.table_names()}")
 
-print("\n=== 3 get specific table information ===")
+print("\n--- 3 get specific table information ---")
 ankauf_anfragen = db.Table(
     "ankauf_anfragen", metadata, autoload=True, autoload_with=engine
 )
 
-print(f"str(ankauf_anfragen): {str(ankauf_anfragen)}")
-print(f"repr(ankauf_anfragen): {repr(ankauf_anfragen)}")
-print(f"ankauf_anfragen.columns.keys(): {ankauf_anfragen.columns.keys()}")
+print(f"{str(ankauf_anfragen)=}")
+print(f"{repr(ankauf_anfragen)=}")
+print(f"{ankauf_anfragen.columns.keys()}=")
 
-print("\n=== 4 - query via sql statements ===")
+print("\n--- 4 - query via sql statements ---")
 
 text = db.text("select * from ankauf_anfragen LIMIT 1000")
 result_proxy = connection.execute(text)
-print(f"type(result_proxy) {type(result_proxy)}")
+print(f"{type(result_proxy)=}")
 # ...or...
 result_proxy = connection.execute("select * from ankauf_anfragen LIMIT 1000")
-print(f"type(result_proxy) {type(result_proxy)}")
+print(f"{type(result_proxy)=}")
 
 results = result_proxy.fetchall()
-print(f"type(results) {type(results)}")
+print(f"{type(results)=}")
 print(results)
 
-print("\n=== 5 - compose a query via functions ===")
+print("\n--- 5 - compose a query via functions ---")
 ankauf_items = db.Table(
     "ankauf_anfragen", metadata, autoload=True, autoload_with=engine
 )
 query = db.select([ankauf_items]).limit(10)  # select * FROM ankauf_items LIMIT 10
-print(f"str(query): {str(query)}")
+print(f"{str(query)=}")
 
 result_proxy = connection.execute(query)
-print(f"type(result_proxy) {type(result_proxy)}")
+print(f"{type(result_proxy)=}")
 
 results = result_proxy.fetchall()
-print(f"type(results) {type(results)}")
+print(f"{type(results)=}")
 print(results)
 
-print("\n=== 6 - insert 1 row ===")
+print("\n--- 6 - insert 1 row ---")
 ankauf_brands = db.Table("ankauf_brands", metadata, autoload=True, autoload_with=engine)
 query = db.insert(ankauf_brands).values(
     brand="Mybrand", counter=69, last_search=datetime.datetime.now()
 )
-print(f"query: {query}")
+print(f"{query=}")
 result_proxy = connection.execute(query)
 query = db.select([ankauf_brands])
-print(f"query: {query}")
+print(f"{query=}")
 results = connection.execute(query).fetchall()
-print(f"Results: {results}")
+print(f"{results=}")
 
 
-print("\n=== 7 insert many rows ===")
+print("\n--- 7 insert many rows ---")
 query = db.insert(ankauf_brands)
 values = [
     {"brand": "my2ndbrand", "counter": 42, "last_search": datetime.datetime.now()},
@@ -96,15 +96,15 @@ values = [
 ]
 result_proxy = connection.execute(query, values)
 query = db.select([ankauf_brands])
-print(f"query: {query}")
+print(f"{query=}")
 results = connection.execute(query).fetchall()
-print(f"Results: {results}")
+print(f"{results=}")
 
-print("\n=== 8 delete rows ===")
+print("\n--- 8 delete rows ---")
 query = db.delete(ankauf_brands)
-print(f"query: {query}")
+print(f"{query=}")
 result_proxy = connection.execute(query, values)
 query = db.select([ankauf_brands])
-print(f"query: {query}")
+print(f"{query=}")
 results = connection.execute(query).fetchall()
-print(f"Results: {results}")
+print(f"{results=}")
