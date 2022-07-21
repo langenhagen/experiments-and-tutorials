8803d590e9abb0f0ea2334d758fa9dfb158c3235
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-07-21 11:07:31 +0200

    Update `exception-raise-from-hello.py`

--- Python/exception-raise-from-hello.py
+++ Python/exception-raise-from-hello.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
-"""Showcase the usage of raise ... from to raise sequences of exceptions."""
+"""
+Showcase the usage of raise ... from to raise sequences of exceptions.
+
+However, `raise from` seems only necessary when you have several exceptions or
+causes you can choose from.
+"""
+
+print("--- 1 raise from catched ---\n")
+try:
+    try:
+        raise ValueError("The first message")
+    except ValueError as err:
+        # following raise logs both the first exception and the second exception
+        raise KeyError("Yep, the second message") from err
+except KeyError as err:
+    print(f"The error says: {err}")
+
+
+print("\n--- 2 simple raise from ---\n")
 
 try:
     raise ValueError("The first message")
-except ValueError as err:
+except ValueError:
     # following raise logs both the first exception and the second exception
-    raise KeyError("Yep, the second message") from err
+    print("Spongebob, no!")
+    raise KeyError("Yep, the second message")
+    # raise KeyError("Yep, the second message") from err  # does the same
