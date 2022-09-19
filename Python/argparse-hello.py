a91daa628cbc13466b20133f1ae9d09480deeca3
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-09-19 18:40:19 +0200

    Improve argparse-hello.py

--- Python/argparse-hello.py
+++ Python/argparse-hello.py
@@ -22,7 +22,7 @@ parser.add_argument(
 )
 parser.add_argument(
     "-b",
-    action="store",
+    action="store",  # "store" is the default action
     dest="b",
     help='help string for "b"',
 )
