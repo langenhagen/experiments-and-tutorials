1afff140e3e482efefa639a5e73011eb87aa5165
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-09-22 16:38:45 +0200

    Improve and modernize dynamic-imports-hello.py

--- Python/dynamic-imports-hello.py
+++ Python/dynamic-imports-hello.py
@@ -4,16 +4,16 @@ Dynamically import the given module.
 
 should work via:
 
-    python hello-dynamic-imports.py treemaps-hello/treemap.py
+    python dynamic-imports-hello.py treemaps-hello/treemap.py
     r = my_module.Rectangle(1,2,3,4)
     print(r)
 """
-import importlib
+# import importlib
+import importlib.util
 import sys
 
-print(f"len(sys.argv) = {len(sys.argv)}")
-print(f"sys.argv = {sys.argv}")
-
+print(f"{sys.argv=}")
+print(f"{len(sys.argv)=}")
 module_path = sys.argv[1]
 print(f"Given Module path: {module_path}")
 
@@ -21,18 +21,14 @@ print(f"Given Module path: {module_path}")
 # my_module = importlib.import_module(module_path)
 
 # Import module via file location
-import importlib.util
-
 module_spec = importlib.util.spec_from_file_location("my.module.name", module_path)
 my_module = importlib.util.module_from_spec(module_spec)
 module_spec.loader.exec_module(my_module)
 
-print("***Module loaded***")
+print("*** Module loaded ***")
 
-print()
-print(f"dir({my_module})")
-print(dir(my_module))
+print(f"{dir(my_module)=}\n---")
+print(f"my_module.__doc__:\n{my_module.__doc__}---\n")
 
-print()
-print(f"{my_module}.__doc__")
-print(my_module.__doc__)
+r = my_module.Rectangle(1, 2, 3, 4)
+print(r)
