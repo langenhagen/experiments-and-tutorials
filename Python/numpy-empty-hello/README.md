caf1a6bf8f31f8831860c9459fb3f4265359a3e5
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-11-16 18:14:52 +0100

    Add numpy-empty-hello/

---
+++ Python/numpy-empty-hello/README.md
@@ -0,0 +1,9 @@
+# GenICam Harvesters Hello
+Showcase the usage of the package `Harvesters` to get images from a GenICam standard camera.
+
+I tested it against an IDS U3-304xCP-C and an Allied Vision Alvium U-508c.
+
+
+See:
+- https://github.com/genicam/harvesters
+- https://github.com/genicam/harvesters/issues/117
---
+++ Python/numpy-empty-hello/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Fun and games with random uninitialized values."""
+import sys
+from itertools import count
+
+import numpy as np
+import cv2
+
+def main() -> int:
+    """Program main entry point."""
+    for i in count(0):
+        key = cv2.waitKey(1)
+        if key == ord("q"):
+            break
+        elif key == 81:  # left key
+            pass
+        elif key == 83:  # right key
+            pass
+        elif key == 82:  # up key
+            pass
+        elif key == 84:  # down key
+            pass
+
+        data = np.empty(shape=(30,30,3), dtype=float)
+        cv2.imshow("image", data)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
