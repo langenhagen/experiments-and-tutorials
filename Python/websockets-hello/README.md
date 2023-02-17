8707413c33cb12271cda73ba8db6cfe4fb747a9e
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2023-02-17 12:15:59 +0100

    Add Python/websockets-hello

---
+++ Python/websockets-hello/README.md
@@ -0,0 +1,10 @@
+# Websockets Hello
+Showcase the usage of the third-party library `websockets`
+
+https://pypi.org/project/websockets/
+
+
+Also the library provides an interactive client:
+```python
+python -m websockets ws://localhost:8765/
+```
---
+++ Python/websockets-hello/client_main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Showcase a simple websockets client.
+
+Taken from: https://websockets.readthedocs.io/"""
+
+import asyncio
+import websockets
+
+async def hello():
+    async with websockets.connect("ws://localhost:8765") as websocket:
+        await websocket.send("Hello world!")
+        await websocket.recv()
+
+asyncio.run(hello())
