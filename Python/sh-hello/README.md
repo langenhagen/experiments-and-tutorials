3649a5651dced924db9524a9c696825665ddeee7
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2023-03-14 18:29:29 +0100

    Ad Python/sh-hello

---
+++ Python/sh-hello/README.md
@@ -0,0 +1,5 @@
+# Hello SH
+Proof of concept for working with the 3rd party Python package `sh`.
+
+- https://amoffat.github.io/sh/
+- https://pypi.org/project/sh/
---
+++ Python/sh-hello/sh-git-hello.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Showcase the usage of the git subpackage of the 3rd party package `sh`."""
+# import sh
+from sh import git
+
+
+def main():
+    """Program entry point."""
+
+    print("--- 1 git status ---")
+    status = git.status()  # sane as git("status")
+    print("status:")
+    print(status)
+
+    porcelain_status = git.status("--porcelain")
+    print(f"{porcelain_status=}")
+
+    diff_files = git.diff("--numstat", "HEAD..HEAD~1")
+    print("diff_files:")
+    print(diff_files)
+
+    print("--- 2 diffs can be problematic with diff-so-fancy - would hang ---")
+
+
+if __name__ == "__main__":
+    main()
