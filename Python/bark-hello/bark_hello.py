2fc8a5eea31d7a401a6e297b192e9f72877e310b
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2023-05-05 01:02:23 +0200

    Update Python/bark-hello

--- Python/bark-hello/bark_hello.py
+++ Python/bark-hello/bark_hello.py
@@ -28,10 +28,13 @@ ValueError: ERROR, something went wrong
 
 """
 from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
 from IPython.display import Audio
 
 # download and load all models
 preload_models()
+
+print(f"{SAMPLE_RATE=}")
 
 # generate audio from text
 text_prompt = """
@@ -41,4 +44,8 @@ text_prompt = """
 audio_array = generate_audio(text_prompt)
 
 # play text in notebook
-Audio(audio_array, rate=SAMPLE_RATE)
+# Audio(audio_array, rate=SAMPLE_RATE)
+
+
+# write audio to disk
+write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
