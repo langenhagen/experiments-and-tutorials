2e8bab40fb9d73e7808d29eea72065381f502dc7
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2023-06-05 13:19:26 +0200

    Add deepcopy-does-not-call-init.py

---
+++ Python/deepcopy-does-not-call-init.py
@@ -0,0 +1,18 @@
+"""Showcase that deepcopy() appears to not call a class's `__init__()`"""
+from copy import deepcopy
+
+class C:
+    my_class_var = 0
+    def __init__(self):
+        print("Hi!")
+        C.my_class_var +=1
+
+
+
+assert C.my_class_var == 0
+
+c = C()
+assert C.my_class_var == 1
+
+d = deepcopy(c)  # no "Hi!"
+assert C.my_class_var == 1
