9598af1b8be7ebe8462a0bbfc87a6edfa5063741
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2023-07-31 18:15:34 +0200

    Add decorators-hello.py

---
+++ Python/decorators-hello.py
@@ -0,0 +1,22 @@
+"""Showcase Python's decorators"""
+from typing import Callable
+
+
+def my_decorator(function: Callable) -> Callable:
+    def wrap(*args, **kwargs):
+        print("before")
+        res = function(*args, **kwargs)
+        print("after")
+        return res
+
+    return wrap
+
+
+class C:
+    @my_decorator
+    def foo(self):
+        print("Hello there!")
+
+
+c = C()
+c.foo()
