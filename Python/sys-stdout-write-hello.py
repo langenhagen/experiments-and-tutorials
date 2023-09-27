bda697062909128b11e1d6466fdaf50188d5dbe5
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2023-09-27 19:41:37 +0200

    Extend sys-stdout-write-hello.py

--- Python/sys-stdout-write-hello.py
+++ Python/sys-stdout-write-hello.py
@@ -1,7 +1,12 @@
 """Showcase an acceptable way to write stuff to stdout that is not `print()`."""
 import sys
 
-print("--- 1 ---\n")
+print("--- 1 write() ---\n")
 
 sys.stdout.write("Hello World,\nThis is a test.")
 sys.stdout.write(" This goes onto the same line")
+
+print("\n--- 2 writelines() ---\n")
+
+lines = ["Does", "Not", "break", "lines", "\nautomatically"]
+sys.stdout.writelines(lines)  # no automatic line breaks
