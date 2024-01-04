b45003afa258226ec7b54b7b643eb25e63d76580
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2024-01-04 18:06:57 +0100

    Add trie-hello.py

---
+++ Python/trie-hello.py
@@ -0,0 +1,70 @@
+"""Showcase the "trie" data structure.
+
+A trie is a tree-based data structure designed for efficient storage and
+retrieval of strings, where each node represents a character, forming paths that
+represent keys. It is commonly employed in tasks such as autocomplete and spell
+checking.
+
+author: andreasl
+"""
+
+
+class TrieNode:
+    """Node for the Trie data structure."""
+
+    def __init__(self) -> None:
+        """Initialize an empty TrieNode."""
+        self.children: dict[str, TrieNode] = {}
+        self.is_end_of_word: bool = False
+
+
+class Trie:
+    """Trie data structure."""
+
+    def __init__(self) -> None:
+        """Initialize an empty Trie."""
+        self.root: TrieNode = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """Insert a word into the Trie."""
+        node: TrieNode = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def search(self, word: str) -> bool:
+        """Search for a word in the Trie."""
+        node: TrieNode = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
+
+    def any_word_start_with_prefix(self, prefix: str) -> bool:
+        """Check if any word in the Trie starts with the given prefix."""
+        node: TrieNode = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+def main() -> None:
+    """Trie experiment main function."""
+    trie: Trie = Trie()
+    trie.insert("apple")
+    trie.insert("app")
+    trie.insert("apricot")
+
+    print(f'{trie.search("app")=}')  # True
+    print(f'{trie.search("orange")=}')  # False
+    print(f'{trie.any_word_start_with_prefix("ap")=}')  # True
+    print(f'{trie.any_word_start_with_prefix("at")=}')  # False
+
+
+if __name__ == "__main__":
+    main()
