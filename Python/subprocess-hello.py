399b90102c8d34ba112928802229db45ad6445c9
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2024-03-28 21:12:01 +0100

    Improve subprocess-hello.py

--- Python/subprocess-hello.py
+++ Python/subprocess-hello.py
@@ -3,16 +3,26 @@
 import os
 import subprocess
 
+print("--- 1 run a command ---\n")
+
 # Run a command and get the output
 os.chdir("../../scripts")
 
-# shell=True: spawn a shell and run the command there
-#             for some weird reason, this has to be set when using
-#             command line options
+# shell=True: spawn a shell and run the command there. for some weird reason,
+# this has to be set when using command line options, otherwise you have to put
+# the options in an array.
+#
 # stdout=subprocess.PIPE: capture the output for later usage
-result = subprocess.run("git log --oneline", shell=True, stdout=subprocess.PIPE)
+result = subprocess.run("git log --oneline -5", shell=True, stdout=subprocess.PIPE)
 
-print(f"Return code: {result.returncode}")
+print(f"{result.returncode=}\n")
 # apparently, the output is utf-8 encoded, so, decode it prior to reading
 print(result.stdout.decode("utf-8"))
 print(result.stderr)  # Probably None
+
+
+print("\n--- 2 subprocess.run() is blocking ---\n")
+
+subprocess.run(["sleep", "2"])
+
+print('Done')
