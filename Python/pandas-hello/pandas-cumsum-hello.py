987d42da4668c0e8d0b6aed752c01b8856c82581
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2024-05-23 12:10:09 +0200

    Showcase pandas dataframe logging

--- Python/pandas-hello/pandas-cumsum-hello.py
+++ Python/pandas-hello/pandas-cumsum-hello.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
-"""Showcase the usage of pandas.Series.cumsum()"""
+"""Showcase the usage of pandas.Series.cumsum().
+
+Also showcase that logging a dataframe prints it nicely."""
+import logging
+
 import pandas as pd
 
+logging.basicConfig(
+    format="[%(levelname)s]: %(filename)s:%(lineno)d: %(message)s",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger(__name__)
+
 df = pd.DataFrame({"a": [1, 2, 1, 3]})
-print("Dataframe:")
-print(df)
+logger.info("Dataframe:\n%s", df)
 
 c = df["a"].cumsum()
-print("Dataframe with cumsum on column 'a':")
-print(c)
+logger.info("Dataframe with cumsum on column 'a':\n%s", c)
