b96bf94f38517e3fc35159d6faae63371edaa437
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2024-06-17 13:25:27 +0200

    Extend dataclasses-hello.py

--- Python/dataclasses-hello.py
+++ Python/dataclasses-hello.py
@@ -10,3 +10,13 @@ class MyClass:
     s: str = "default string"
     class_var: ClassVar[int] = 0
     a_list: list[str] = field(default_factory=list)
+
+    def __post_init__(self):
+        """An optional place for additional validation."""
+        print("Hello from _post_init!")
+
+
+print("--- 1 dataclasses ---")
+
+c = MyClass(42)
+print(c)
