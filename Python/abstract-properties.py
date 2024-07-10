bfb5c0af722929ac1bcd28203cc43d0dcbb8a328
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2024-07-10 11:44:58 +0200

    Add abstract-properties.py

---
+++ Python/abstract-properties.py
@@ -0,0 +1,22 @@
+"""Showcase properties in Python, especially defined in an abstract base class."""
+from abc import ABC, abstractmethod
+
+
+class MyBase(ABC):
+    @property
+    @abstractmethod
+    def my_property(self):
+        pass
+
+
+class C(MyBase):
+    def __init__(self, value):
+        self._value = value
+
+    @property
+    def my_property(self):
+        return self._value
+
+
+c = C(42)
+print(c.my_property)  # 42
