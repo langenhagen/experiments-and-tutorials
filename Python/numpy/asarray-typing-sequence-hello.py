a3216581ffd91978b7ba07ba313fd0fcb1457a0f
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2024-08-20 12:46:26 +0200

    Add some numpy experiments

---
+++ Python/numpy/asarray-typing-sequence-hello.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Showcase np.asarray with typing.Sequence.
+
+Fyi, ndarray is not a Sequence: https://stackoverflow.com/questions/76082807/why-numpy-array-is-not-of-sequence-type.
+"""
+
+from typing import Sequence, TYPE_CHECKING
+import numpy as np
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+
+def to_numpy_array(seq: Sequence[float]) -> NDArray:
+    return np.asarray(seq)
+
+
+# Example usage:
+my_list = [1.0, 2.0, 3.0, 4.0]  # This is a Sequence[float]
+array = to_numpy_array(my_list)
+
+print(array)  # Output: [1. 2. 3. 4.]
+print(type(array))  # Output: <class 'numpy.ndarray'>
---
+++ Python/numpy/typing-ndarray-hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Showcase np.asarray with numpy.typing.NDArray."""
+import numpy as np
+from numpy.typing import NDArray
+
+
+def array_mean(arr: NDArray[np.float64]) -> float:
+    """Calculate the mean of a NumPy array with float64 elements."""
+    return arr.mean()
+
+
+# Example usage:
+my_array = np.array([1.0, 2.0, 3.0, 4.0], dtype=np.float64)
+mean_value = array_mean(my_array)
+
+print(mean_value)  # Output: 2.5
