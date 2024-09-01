13f3ae8fb78fae331682c7210ba469abd2478326
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2024-09-02 01:38:45 +0200

    Add Python/guppy3-hello

---
+++ Python/guppy3-hello/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Showcase the usage of the 3rd party `guppy3` library.
+
+Provides tools for heap memory analysis, useful for profiling and debugging
+memory usage.
+
+See: https://pypi.org/project/guppy3/
+
+author: andreasl
+"""
+from guppy import hpy
+
+print("\n--- 1 inspect current memory heap ---\n")
+
+h = hpy()  # create a heap instance
+print("Heap info:")
+print(f"{h.heap()=}")
+
+print("\n--- 2 monitor memory growth ---\n")
+
+l = list(range(100_000))
+
+print("Heap after allocation:")
+print(f"{h.heap()=}")
+
+print("\n--- 3 analyze memory usage by object ---\n")
+
+print("Memory usage by object:")
+print(h.iso(l))  # iso[late] object l
