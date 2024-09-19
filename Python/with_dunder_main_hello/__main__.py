bbaafd2aa715fc4b0aa213cf005b09a59d083662
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2024-09-19 21:53:55 +0200

    Add Python/with_dunder_main_hello

---
+++ Python/with_dunder_main_hello/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Showcase that executing a folder with a `__main__.py` file works in Python.
+
+Run from parent dir via:
+
+    python with_dunder_main_hello
+
+
+author: andreasl
+"""
+
+print("Hi there!")
