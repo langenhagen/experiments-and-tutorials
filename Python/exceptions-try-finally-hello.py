40627285980e9962f73b837849d2d870a4106d9f
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2024-10-02 15:35:42 +0200

    Extend exceptions-try-finally-hello.py

--- Python/exceptions-try-finally-hello.py
+++ Python/exceptions-try-finally-hello.py
@@ -1,18 +1,54 @@
 #!/usr/bin/env python3
-"""Showcase the usage of less common try-clauses."""
+"""Showcase the usage of less common try-clauses.
 
-print("--- 1 try-finally ---")
+Comment out the functions you don't want to see crashing, so you can see the
+program crashing in places that you are interested in.
+"""
 
-try:
-    raise ValueError("hahaha")
-finally:
-    print("This will happen before the exception crashes the program")
+print("--- 1 try-finally ---\n")
 
 
-# Does the following:
-#
-# This will happen before the exception crashes the program
-# Traceback (most recent call last):
-#   File "<input>", line 2, in <module>
-#     raise ValueError("hahaha")
-# ValueError: hahaha
+def try_finally():
+    # Does the following:
+    #
+    # This will happen before the exception crashes the program
+    # Traceback (most recent call last):
+    #   File "<input>", line 2, in <module>
+    #     raise ValueError("hahaha")
+    # ValueError: hahaha
+    try:
+        raise ValueError("hahaha")
+    finally:
+        print("Hello from the finally clause: happens before program crashes")
+
+
+# try_finally()
+
+print("\n--- 2 try-except-finally ---\n")
+
+
+def try_except_finally():
+    try:
+        raise ValueError("hahaha")
+    except ValueError as e:
+        print(f"Caught a Value Error: {e}")
+    finally:
+        print("Hello from the finally clause!")
+
+
+try_except_finally()
+
+print("\n--- 3 try-except-raise-finally - finally kicks in always ---\n")
+
+
+def try_except_finally():
+    try:
+        raise ValueError("hahaha")
+    except ValueError as e:
+        print(f"Caught a Value Error: {e}, raising again!")
+        raise
+    finally:
+        print("Hello from the finally clause!")
+
+
+try_except_finally()
