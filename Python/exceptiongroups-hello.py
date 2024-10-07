e3523851f44e0590ca6af6f8366ad5a705fda890
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2024-10-07 18:50:18 +0200

    Extend exceptiongroups-hello.py

--- Python/exceptiongroups-hello.py
+++ Python/exceptiongroups-hello.py
@@ -74,3 +74,33 @@ except ExceptionGroup as eg:
     print(f"Caught ExceptionGroup with {len(eg.exceptions)} exceptions:")
     for exc in eg.exceptions:
         print(f" - {exc.__class__.__name__}: {exc}")
+
+
+print("\n--- 3 except* syntax ---\n")
+
+
+def raise_combined_exceptions():
+    """Raise an ExceptionGroup with a mix of different exceptions."""
+    exceptions = [
+        ZeroDivisionError("ZeroDivision"),
+        ZeroDivisionError("Other ZeroDivision"),
+        KeyError("KeyError"),
+        ValueError("ValueError"),
+    ]
+    raise ExceptionGroup("Combined exception group", exceptions)
+
+
+try:
+    raise_combined_exceptions()
+except* ZeroDivisionError as zde:
+    print(f"Caught ZeroDivisionError(s): {zde}")
+    for exc in zde.exceptions:
+        print(f" - Sub-exception: {exc}")
+except* KeyError as ke:
+    print(f"Caught KeyError(s): {ke}")
+    for exc in ke.exceptions:
+        print(f" - Sub-exception: {exc}")
+except* ValueError as ve:
+    print(f"Caught ValueError(s): {ve}")
+    for exc in ve.exceptions:
+        print(f" - Sub-exception: {exc}")
