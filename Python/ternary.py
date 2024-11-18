d99c22eef03d0e2b4b88995a26198f6c11127a44
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2024-11-18 18:01:22 +0100

    Add ternary.py

---
+++ Python/ternary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Showcase how Python is a bit messy with comparison operators when applied one after another,
+apparently treating them as ternary operations"""
+
+assert (5 > 4) == True
+assert 5 > (4 == True)
+
+# assert 5 > 4 == True   # fails
+# assert 5 > 4 == False  # fails as well
+assert 5 > 4 == 4
