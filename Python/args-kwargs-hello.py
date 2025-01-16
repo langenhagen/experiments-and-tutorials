6f4bef31c179e6c509239233b4428322a0aad56a
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2025-01-16 16:28:24 +0100

    Extend args-kwargs-hello.py

--- Python/args-kwargs-hello.py
+++ Python/args-kwargs-hello.py
@@ -2,13 +2,27 @@
 """Showcase the usage of args and kwargs."""
 
 
+def my_purely_args_function(*args):
+    """Showcase the implementation of a pure args function."""
+    print(f"{args=}")
+    print(f"{type(args)=}")
+
+
 def my_purely_kwargs_function(**kwargs):
     """Showcase the implementation of a pure kwargs function."""
     print(f"{kwargs=}")
     print(f"{type(kwargs)=}")
 
 
-print("--- 1 pure kwargs ---\n")
+print("--- 1 pure args ---\n")
+
+print("Without args")
+my_purely_args_function()
+
+print("With args")
+my_purely_args_function(23, "Jorge")
+
+print("\n--- 2 pure kwargs ---\n")
 
 print("Without kwargs")
 my_purely_kwargs_function()
