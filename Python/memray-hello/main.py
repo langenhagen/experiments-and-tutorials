e1d19928d8f4edd5ec46c8559c6e99b4704573af
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2025-02-03 20:14:23 +0100

    Update and extend memray-hello

--- Python/memray-hello/main.py
+++ Python/memray-hello/main.py
@@ -2,6 +2,14 @@
 """
 Showcase the usage of the 3rd party tool `memray` that can help track memory usage.
 
+Note:
+When it comes to details, Memray seems to be somewhat unreliable, especially
+with transient memory hogs. Sometimes also seems to report wrong values. In this
+little example, it seems to report back the biggest memory hog in every frame
+proper, but the second and subsequent highest hogs get either not reported or
+reported with wrong values. I'd take what Memray gives me back with a grain of
+salt.
+
 Example usage:
 
     rm *.bin; rm *.html;
@@ -11,7 +19,13 @@ Example usage:
 
 author: andreasl
 """
+
 from dataclasses import dataclass
+from time import sleep
+
+
+def hog_memory(n: int):
+    return [i for i in range(n)]
 
 
 @dataclass
@@ -19,11 +33,21 @@ class C:
     i: int
 
 
-N = 10_000_000
+# N = 10_000_000
+N = 1_000_000
 
+# Not-so transient memory hogging
 l = [C(i=i) for i in range(N)]
-
+# sleep(1)
 for i in range(N):
-    del l[10_000_000 - 1 - i]
+    del l[N - 1 - i]
+
+# Basically tranient memory hogging
+x = hog_memory(N)
+# sleep(1)
+del x
+
+# Very much transient memory hoggin
+[i for i in range(N)]
 
 print("END")
