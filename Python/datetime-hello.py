76f245069af5b2ce72010d8be0cafbe2eeeb560c
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2025-06-01 13:40:30 +0200

    Improve datetime-hello.py

--- Python/datetime-hello.py
+++ Python/datetime-hello.py
@@ -5,28 +5,33 @@ import datetime as dt
 
 print("--- 1 - now ---\n")
 now = dt.datetime.now()
+# naive: datetime.datetime(2025, 6, 1, 11, 37, 7, 649943)
 utcnow = now.utcnow()
-utcnow2 = dt.datetime.now(tz=dt.timezone.utc)
+# better version: datetime.datetime(2025, 6, 1, 11, 38, 6, 582678, tzinfo=datetime.timezone.utc)
+now_with_tz_utc = dt.datetime.now(tz=dt.UTC)
 
 print(f"{now=}")
 print(f"{utcnow=}")
+print(f"{now_with_tz_utc=}")
 
 print("\n--- 2 - timezones ---\n")
+
 print(f"{now.utcoffset()=}")
 print(f"{utcnow.utcoffset()=}")
-print(f"{utcnow2.utcoffset()=}")
+print(f"{now_with_tz_utc.utcoffset()=}")
 
 print("\n--- 3 - datetime differences ---\n")
+
 # if your machine is not in UTC, there will be a difference since both now and
 # utcnow have no timezone
 print(f"{now - utcnow=}")
 
 try:
-    print(f"{utcnow - utcnow2=}")
+    print(f"{utcnow - now_with_tz_utc=}")
 except TypeError:  # can't subtract offset-naive and offset-aware datetimes
     print(
         "Exception: can't subtract offset-naive and offset-aware datetimes"
-        f"{utcnow=} and {utcnow2=}"
+        f"{utcnow=} and {now_with_tz_utc=}"
     )
 
 print("\n--- 4 string from time ---\n")
