8a0fe5e77a851497c8ea3279b14d4fad18cc49c8
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2025-06-02 18:38:03 +0200

    Improve attrs-hello

--- Python/attrs-hello/attrs-hello.py
+++ Python/attrs-hello/attrs-hello.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Showcase basic usage of the 3rd party library `attrs`.
 
-@author: andreasl
-@version: 2021-02-08
+author: andreasl
+version: 2021-02-08
 """
 
 import attr
@@ -16,7 +16,7 @@ class MyAttrClass:
     my_other_field = attr.ib(default=None)
 
 
-print("--- 1 simple usage ---")
+print("--- 1 simple usage ---\n")
 o = MyAttrClass(42, "answer to wh0t")
 print(f"{o=}")
 print(f"{o.my_field=}")
@@ -26,11 +26,11 @@ p = MyAttrClass(32)
 print(f"{p=}")
 # q = MyAttrClass()  # doesn't work
 
-print("--- 2 convert attr class to dict ---")
+print("\n--- 2 convert attr class to dict ---\n")
 d = attr.asdict(o)
 print(d)
 
-print("--- 3 copy an object from another object ---")
+print("\n--- 3 copy an object from another object ---\n")
 o2 = attr.evolve(o)
 print(f"{o2=}")
 print(f"{o2.my_field=}")
