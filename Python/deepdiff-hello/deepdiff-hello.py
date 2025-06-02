70c53bd59628f06e50fd31f47abeaed836bbef6b
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2025-06-03 01:08:26 +0200

    Extend deepdiff-hello.py

--- Python/deepdiff-hello/deepdiff-hello.py
+++ Python/deepdiff-hello/deepdiff-hello.py
@@ -10,23 +10,25 @@ author: andreasl
 from deepdiff import DeepDiff
 from pydantic import BaseModel
 
-print("--- 1 simple DeepDiff ---\n")
+print("--- 1 simple DeepDiff with dicts ---\n")
 
 d = {
     "my": {
         "nested": "dict",
         "value": 42,
+        "timestamp": 123,
     },
 }
 e = {
     "my": {
         "other": "dict",
         "value": 42,
+        "timestamp": 345,
     },
 }
 print(f"{DeepDiff(d, e) = }")
 
-print("\n--- 2 DeepDiff with Pydantic models ---\n")
+print("\n--- 2 with Pydantic models ---\n")
 
 
 class Nested(BaseModel):
@@ -45,15 +47,18 @@ n = MyModel(status="nok", value=42, nested=Nested(v=2, w=2))
 
 print(f"{DeepDiff(m, n) = }")
 
-print("\n--- 2 DeepDiff exclude_paths with Pydantic models ---\n")
+print("\n--- 2 exclude_paths with Pydantic models ---\n")
 
 exclude_paths = ["root.status", "root.nested.v"]
-print(f"{DeepDiff(m, n, exclude_paths=["root.status"]) = }")
-print(f"{DeepDiff(m, n, exclude_paths=["root.nested.v"]) = }")
+print(f"{DeepDiff(m, n, exclude_paths=["status"])      = }")
+print(f"{DeepDiff(m, n, exclude_paths=["root.status"]) = }")  # just the same
+
+print(f"{DeepDiff(m, n, exclude_paths=["nested.v"])      = }")
+print(f"{DeepDiff(m, n, exclude_paths=["root.nested.v"]) = }")  # just the same
+
 print(f"{DeepDiff(m, n, exclude_paths=["root.status", "root.nested.v"]) = }")
 
-
-print("\n--- 3 DeepDiff exclude_paths with lists of Pydantic models ---\n")
+print("\n--- 3 exclude_regex_paths with lists of Pydantic models ---\n")
 
 l = [Nested(v=1, w=2), Nested(v=2, w=3), Nested(v=3, w=4)]
 k = [Nested(v=4, w=2), Nested(v=5, w=3), Nested(v=6, w=5)]
@@ -61,4 +66,28 @@ k = [Nested(v=4, w=2), Nested(v=5, w=3), Nested(v=6, w=5)]
 print(f"{DeepDiff(l, k) = }")
 
 # ignore all .v's
-print(f"{DeepDiff(l, k, exclude_regex_paths=[r"root\[\d+\].v"]) = }")
+my_regex = r"root\[\d+\].v"
+print(f"{DeepDiff(l, k, exclude_regex_paths=[my_regex]) = }")
+
+
+print("\n--- 4 exclude_paths with dicts ---\n")
+
+print(f"{DeepDiff(d, e, exclude_paths=["root['my']['timestamp']"]) = }")
+
+print(f"{DeepDiff(d, e, exclude_paths=["root['my']['timestamp']","root['my']['nested']","root['my']['other']"]) = }")
+
+
+print("\n--- 5 dict against None works ---\n")
+
+print(f"{DeepDiff(None, d) =}")
+
+print("\n--- 5 Pydantic model against None with exclude works ---\n")
+
+print(f"{DeepDiff(None, Nested(v=11, w=12), exclude_paths=['v'])}")
+
+print("\n--- 6 Falsiness of DeepDiff when identical objects ---\n")
+
+if DeepDiff(None, None):
+    print("Deepdiff is truthy")
+else:
+    print("Deepdiff is falsy")  # this holds
