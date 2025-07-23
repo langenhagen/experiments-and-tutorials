a42c489ae6d0905386931635bd2bb3ab6136c290
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2025-07-23 23:57:44 +0200

    Improve pydantic-hello

--- Python/pydantic-hello/main.py
+++ Python/pydantic-hello/main.py
@@ -22,19 +22,33 @@ class Address(BaseModel):
     zip_code: str
 
 
+class Status(str, Enum):
+    """Enum representing various statuses."""
+
+    ACTIVE = "active"
+    INACTIVE = "inactive"
+
+
 class Person(BaseModel):
     """A simple Pydantic model for a person."""
 
     name: str
     age: int
+    status: Status
     address: Address
     hobbies: list[str] = Field(default_factory=list)
+
+    @property
+    def myprop(self) -> float:
+        """Won't ever be included in dumps."""
+        return 42
 
 
 a = Person(
     name="Alice",
     age=30,
     hobbies=["reading", "hiking"],
+    status=Status.ACTIVE,
     address=Address(
         street="123 Maple St",
         city="Wonderland",
@@ -49,6 +63,7 @@ try:
     b = Person(
         name="Bob",
         age="thirty",  # error: this should be an int or int-parseable
+        status=Status.ACTIVE,
         address=Address(
             street="456 Oak St",
             city="Springfield",
@@ -64,6 +79,7 @@ c = Person(
     name="Charlie",
     age="40",  # hand in age number as a string, gets parsed automatically
     hobbies=["gaming", "coding"],
+    status="active",  # hand in a string, get according enum value
     address=Address(
         street="789 Pine St",
         city="Gotham",
@@ -74,66 +90,31 @@ print(c)
 
 print("\n--- 4 recursive JSON serialization of individual models---\n")
 
-aj = a.model_dump_json()
+aj = (
+    a.model_dump_json()
+)  # same as `.model_dump(mode="json")`; can also json-serialize nested enums
 
 print(f"{type(aj)=}\n")
 print(aj)
 
 print("\n--- 5 recursive to dict ---\n")
 
-ad = a.model_dump()
+ad = a.model_dump()  # retains the enum; not JSON serializable
 
 print(f"{type(ad)=}\n")
 print(json.dumps(ad, indent=4))
 
-print("\n--- 6 take care when dumping objects with Enum values ---\n")
+print("\n--- 6 model_dump(include=...) omits everything not included ---\n")
 
-
-class Status(str, Enum):
-    """Enum representing various statuses."""
-
-    ACTIVE = "active"
-    INACTIVE = "inactive"
-
-
-class Worker(BaseModel):
-    """A model with an enum field."""
-
-    name: str
-    status: Status
-    tasks_completed: int
-
-    # Won't be included
-    @property
-    def myprop(self) -> float:
-        return 42
-
-
-w = Worker(
-    name="Dave",
-    status=Status.ACTIVE,
-    tasks_completed=15,
-)
-
-print(f"{w=}")
-
-wd1 = w.model_dump()
-print(f"{wd1=}")  # retains the enum; not JSON serializable
-
-wd2 = w.model_dump(mode="json")
-print(f"{wd2=}")  # serializes the enum as well
-
-
-print("\n--- 7 model_dump(include=...) omits everything not included ---\n")
-
-print(f"{w=}")
-
-wd3 = w.model_dump(mode="json", include={"name", "myprop"})
-print(f"{wd3=}")
+ad2 = a.model_dump(
+    mode="json", include={"name", "myprop"}
+)  # will only contain `name` field
+print(f"{ad2=}")
 
 print("\n--- 8 introspection ---\n")
 
 print(f"{Person.model_fields=}")
 
 print("\n--- 9 interactive playground ---\n")
+
 embed(locals_=locals(), banner="\nDropping to interactive shell\n")
