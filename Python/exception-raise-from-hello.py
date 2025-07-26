7bb2c7ccba79d233667ee965eda515b3c7aea0ff
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2025-07-26 13:19:27 +0200

    Extend & improve exception-raise-from-hello.py

--- Python/exception-raise-from-hello.py
+++ Python/exception-raise-from-hello.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Showcase the usage of raise ... from to raise sequences of exceptions.
 
-However, `raise from` seems only necessary when you have several exceptions or
-causes you can choose from.
+However, `raise from` is only necessary when you have several exceptions or
+causes you can choose from, e.g. in nested try/except blocks.
 """
 
-print("--- 1 raise from caught ---\n")
+print("--- 1 the last thrown exception gets propagated ---\n")
 try:
     try:
         raise ValueError("The first message")
@@ -16,12 +16,24 @@ except KeyError as err:
     print(f"The error says: {err}")
 
 
-print("\n--- 2 simple raise from ---\n")
+# print("\n--- 2 simple `raise from` is unnecessary ---\n")
+
+# try:
+#     raise ValueError("The first message")
+# except ValueError:  # except ValueError as err is not necessary
+#     # following raise logs both the first exception and the second exception
+#     print("Spongebob, no!")
+#     raise KeyError("Yep, the second message")
+#     # raise KeyError("Yep, the second message") from err  # does the same
+
+print("\n--- 3 valid `raise from` usecase ---\n")
 
 try:
-    raise ValueError("The first message")
-except ValueError:  # except ValueError as err is not necessary
-    # following raise logs both the first exception and the second exception
-    print("Spongebob, no!")
-    raise KeyError("Yep, the second message")
-    # raise KeyError("Yep, the second message") from err  # does the same
+    raise ValueError("¡Dios mío!")
+except ValueError as verr:
+    try:
+        raise KeyError("¡Ay caramba!")
+    except KeyError:
+        raise AssertionError("verr Qué padre") from verr
+        # raise AssertionError("kerr Qué padre") from kerr
+        # raise AssertionError("automatico Qué padre")  # same as ... from kerr
